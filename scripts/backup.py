44643d5899186d8697cc7c069e856423e427205e
jrasmusbm <JRasmusBm@gmail.com>
2019-07-19 18:01:20 +0200

    Fix logging

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,4 +1,4 @@
-from setup_config import config
+from setup_config import config, Config
 from pathlib import Path
 
 
@@ -24,10 +24,12 @@ def backup(dir_path, overwrite=False):
 def restore(dir_path):
     backup_dir = Path(dir_path).expanduser().resolve()
     log_file = backup_dir / ".backup.log"
-    for path in config.symlinks:
+    backup_config = Config(log_file)
+    log = []
+    for path in backup_config.file_mappings:
         backup_path = backup_dir / path
         if backup_path.exists():
-            original_path = Path.home() / path
+            original_path = Path.home() / backup_config.file_mappings[path]
             if not original_path.is_symlink() and original_path.exists():
                 accept = False
                 choice = input(
@@ -41,8 +43,11 @@ Overwrite [Y/n]?
                 """
                 )
                 if choice not in ["Y", "y"]:
+                    log.append(f"{ backup_path },{ original_path }")
                     continue
             backup_path.rename(original_path)
+    with open(log_file, "w") as f:
+        f.write("\n".join(log))
 
 
 def symlink():
--- scripts/setup_config.py
+++ scripts/setup_config.py
@@ -2,19 +2,12 @@ from pathlib import Path
 
 
 class Config:
-    def __init__(self):
-        try:
-            with open("../files_to_copy.txt") as f:
-                raw = f.read()
-        except:
-            with open("./files_to_copy.txt") as f:
-                raw = f.read()
+    def __init__(self, file_name):
+        with open(file_name) as f:
+            raw = f.read()
 
         self._link_pairs = [
-            tuple(
-                Path(file_name)
-                for file_name in file_info.split(",")
-            )
+            tuple(Path(file_name) for file_name in file_info.split(","))
             for file_info in raw.split("\n")
             if file_info
         ]
@@ -32,7 +25,10 @@ class Config:
         return {link[0]: link[1] for link in self._link_pairs}
 
 
-config = Config()
+try:
+    config = Config("../files_to_copy.txt")
+except:
+    config = Config("./files_to_copy.txt")
 
 if __name__ == "__main__":
     print(config.file_mappings)
