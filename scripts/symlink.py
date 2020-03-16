1ba5ff964536eaed78e3cf358782764272927718
jrasmusbm <JRasmusBm@gmail.com>
2020-03-16 12:52:12 +0100

    Create folders when symlinking

--- scripts/symlink.py
+++ scripts/symlink.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from subprocess import run
 
 from backup import backup
 from file_writer import read_config
@@ -9,6 +10,8 @@ def symlink(backup_folder="~/.backup_dotfiles"):
     for the_file in read_config("./files.csv"):
         original_path = the_file.source
         symlink_path = Path.home() / the_file.target
+        if not symlink_path.parents[0].is_dir():
+            run(["mkdir", "-p", symlink_path.parents[0]])
         if not symlink_path.is_symlink():
             symlink_path.symlink_to(
                 original_path, target_is_directory=the_file.is_dir
