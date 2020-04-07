ec882fdbf8270992c46ea0df0159a4e24bb88b70
jrasmusbm <JRasmusBm@gmail.com>
2020-04-07 10:12:34 +0200

    Use ncspot instead of spotify-client

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -277,11 +277,15 @@ update_system() {
   sudo apt-get autoremove -y
 }
 
-install_spotify() {
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-  echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-  sudo apt-get update
-  sudo apt-get install -y spotify-client
+install_spotify_cli() {
+  sudo apt install -y \
+    libpulse-dev \
+    libssl-dev \
+    libxcb1-dev \
+    libxcb-render0-dev \
+    libxcb-shape0-dev \
+    libxcb-xfixes0-dev
+  cargo install ncspot
 }
 
 install_docker() {
