58f282d6b24f54efc54f6773f6e3ad4430aa64c6
jrasmusbm <JRasmusBm@gmail.com>
2020-09-08 20:47:06 +0200

    Add support for installing brave_stable
    **Why** is the change needed?
    
    The beta version was crashing many times in the course of one day when
    I needed smooth operation.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -210,6 +210,18 @@ load_nvim_plugins() {
 
 # --- Everything above this line is tested ---
 
+install_brave_stable() {
+    sudo apt install -y apt-transport-https curl
+
+    curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+    echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+    sudo apt update
+
+    sudo apt install -y brave-browser
+}
+
 install_rust() {
   curl https://sh.rustup.rs -sSf | sh
 }
