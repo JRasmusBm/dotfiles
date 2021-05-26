2dd20c090c8f9729f204c7e7f6d9f50b431fcc1f
jrasmusbm <jrasmusbm@gmail.com>
2021-05-26 08:21:51 +0200

    Support local .venv folder in pyright
    **Why** is the change needed?
    
    So that it finds the correct types and go to definition etc. works.

--- vim/lua/jrasmusbm/lsp/python.lua
+++ vim/lua/jrasmusbm/lsp/python.lua
@@ -15,11 +15,16 @@ function M.format()
 end
 
 function M.setup(options)
-  local poetry_venv_path =vim.fn.systemlist({ "poetry", "env" ,"info","-p" })[1] .. "/bin/python"
-  local python_path = "python"
+  local poetry_venv_path = vim.fn.systemlist({ "poetry", "env" ,"info","-p" })[1]
+  local venv_path = vim.fn.getcwd() .. "/.venv"
+  local python_path
 
-  if vim.fn.filereadable(poetry_venv_path) then
-    python_path = poetry_venv_path
+  if vim.fn.isdirectory(poetry_venv_path) ~= 0 then
+    python_path = poetry_venv_path .. "/bin/python"
+  elseif vim.fn.isdirectory(venv_path) ~= 0 then
+    python_path = venv_path .. "/bin/python"
+  else
+    python_path = "python3.8"
   end
 
   require'lspconfig'.pyright.setup{
