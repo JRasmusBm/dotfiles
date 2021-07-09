d76a8c3a9c302b7534bfae04ae4c13f1fa82d533
jrasmusbm <jrasmusbm@gmail.com>
2021-07-09 11:05:44 +0200

    Let Sumneko Lua know about plugins
    **Why** is the change needed?
    
    So that I can get LSP help with what I can do with them.
    
    **How** is the need addressed?
    
    Adapted from:
    
    https://github.com/tjdevries/nlua.nvim/blob/a0722bd2f6fdf54f85aaf8dcd6a1032f64c83905/lua/nlua/lsp/nvim.lua#L28-L45

--- vim/lua/jrasmusbm/lsp/lua.lua
+++ vim/lua/jrasmusbm/lsp/lua.lua
@@ -1,29 +1,46 @@
 local M = {}
 
-local lua_language_server = vim.fn.stdpath("cache").."/../lua-language-server"
+local lua_language_server = vim.fn.stdpath("cache") .. "/../lua-language-server"
+
+local function get_lua_runtime()
+    local result = {};
+    for _, path in pairs(vim.api.nvim_list_runtime_paths()) do
+        local lua_path = path .. "/lua/";
+        if vim.fn.isdirectory(lua_path) then result[lua_path] = true end
+    end
+
+    result[vim.fn.expand("$VIMRUNTIME/lua")] = true
+
+    return result;
+end
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
 
 function M.setup(options)
-  require("lspconfig").sumneko_lua.setup{ on_attach=options.on_attach , cmd = {lua_language_server.."/bin/Linux/lua-language-server", "-E", lua_language_server.."/main.lua"};
-    settings = {
-      Lua = {
-        runtime = {
-          version = 'LuaJIT',
-          path = vim.split(package.path, ';'),
+    require("lspconfig").sumneko_lua.setup {
+        on_attach = options.on_attach,
+        cmd = {
+            lua_language_server .. "/bin/Linux/lua-language-server", "-E",
+            lua_language_server .. "/main.lua"
         },
-        diagnostics = {
-          globals = {'vim'},
-        },
-        workspace = {
-          library = {
-            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          },
-        },
-        telemetry = {
-          enable = false,
-        },
-      },
-  },}
+        settings = {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT',
+                    path = runtime_path
+                },
+                diagnostics = {globals = {'vim'}},
+                workspace = {
+                    library = get_lua_runtime(),
+                    maxPreload = 10000,
+                    preloadFileSize = 10000
+                },
+                telemetry = {enable = false}
+            }
+        }
+    }
 end
 
 return M
