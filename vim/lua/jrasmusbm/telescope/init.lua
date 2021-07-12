f85daa00614286dd53d8b306daa97ac31a226922
jrasmusbm <jrasmusbm@gmail.com>
2021-07-12 08:30:20 +0200

    Make changes to telescope config
    **Why** is the change needed?
    
    They made backward-incompatible changes.

--- vim/lua/jrasmusbm/telescope/init.lua
+++ vim/lua/jrasmusbm/telescope/init.lua
@@ -1,28 +1,19 @@
 local actions = require("telescope.actions")
 
-require('telescope').setup{
-  defaults = {
-    prompt_position = "top",
-    sorting_strategy = "ascending",
-    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
-    file_ignore_patterns = {"pack/.*"},
-    shorten_path = true,
-    width = 0.75,
-    preview_cutoff = 120,
-    results_height = 1,
-    results_width = 0.8,
-    mappings = {
-      i = {
-        ["<C-x>"] = false,
-        ["<C-q>"] = actions.send_to_qflist,
-      }
-    }
-  },
-  extensions = {
-    fzy_native = {
-      override_file_sorter = true
-    }
-  }
+require('telescope').setup {
+    defaults = {
+        sorting_strategy = "descending",
+        file_sorter = require'telescope.sorters'.get_fzy_sorter,
+        file_ignore_patterns = {"pack/.*"},
+        path_display = {},
+        layout_config = {
+            prompt_position = "bottom",
+            width = 0.75,
+            preview_cutoff = 120;
+        },
+        mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}}
+    },
+    extensions = {fzy_native = {override_file_sorter = true}}
 }
 
 require("telescope").load_extension("fzy_native")
