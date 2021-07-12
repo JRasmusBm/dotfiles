01dac348da91859a8ae0004cb61b40da02526868
jrasmusbm <jrasmusbm@gmail.com>
2021-07-12 09:17:34 +0200

    Fix issue iwth sorting_startegy="ascending"
    **Why** is the change needed?
    
    It was stopping me from being able to conduct searches.
    
    **How** is the need addressed?
    
    - https://github.com/nvim-telescope/telescope.nvim/issues/840
    - https://github.com/nvim-telescope/telescope.nvim/pull/978

--- vim/lua/jrasmusbm/telescope/init.lua
+++ vim/lua/jrasmusbm/telescope/init.lua
@@ -1,19 +1,19 @@
 local actions = require("telescope.actions")
 
 require('telescope').setup {
-    defaults = {
-        sorting_strategy = "descending",
-        file_sorter = require'telescope.sorters'.get_fzy_sorter,
-        file_ignore_patterns = {"pack/.*"},
-        path_display = {},
-        layout_config = {
-            prompt_position = "bottom",
-            width = 0.75,
-            preview_cutoff = 120;
-        },
-        mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}}
+  defaults = {
+    sorting_strategy = "ascending",
+    file_sorter = require'telescope.sorters'.get_fzy_sorter,
+    file_ignore_patterns = {"pack/.*"},
+    path_display = {},
+    layout_config = {
+      prompt_position = "top",
+      width = 0.75,
+      preview_cutoff = 120;
     },
-    extensions = {fzy_native = {override_file_sorter = true}}
+    mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}}
+  },
+  extensions = {fzy_native = {override_file_sorter = true}}
 }
 
 require("telescope").load_extension("fzy_native")
