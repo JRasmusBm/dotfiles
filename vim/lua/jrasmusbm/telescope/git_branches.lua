53e07e0c1394c99b4deba42464637261847ef58a
jrasmusbm <jrasmusbm@gmail.com>
2021-07-15 20:02:25 +0200

    Add git-commit-tree updates after Telescope actions
    **Why** is the change needed?
    
    So that I can make the changes and see them reflected directly in
    git-commit-tree.
    
    Closes #182

--- vim/lua/jrasmusbm/telescope/git_branches.lua
+++ vim/lua/jrasmusbm/telescope/git_branches.lua
@@ -1,17 +1,41 @@
 local M = {}
 
 local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
 
 M.git_branches = function(options_)
     local options = options_ or {}
-    options["attach_mappings"] = function(_, map)
+    options["attach_mappings"] = function(prompt_bufnr, map)
         local reset_hard = function()
             local selection = action_state.get_selected_entry()
             vim.api.nvim_command('Git rh ' .. selection.value)
+            actions.close(prompt_bufnr)
+            vim.api.nvim_command('call jrasmusbm#git#list_tree()')
+        end
+
+        local checkout = function()
+            actions.git_checkout(prompt_bufnr)
+            vim.api.nvim_command('call jrasmusbm#git#list_tree()')
+        end
+
+        local track = function()
+            actions.git_track_branch(prompt_bufnr)
+            vim.api.nvim_command('call jrasmusbm#git#list_tree()')
+        end
+
+        local delete = function()
+            actions.git_delete_branch(prompt_bufnr)
+            vim.api.nvim_command('call jrasmusbm#git#list_tree()')
         end
 
         map('i', '<C-h>', reset_hard)
         map('n', '<C-h>', reset_hard)
+        map('i', '<CR>', checkout)
+        map('n', '<CR>', checkout)
+        map('i', '<C-d>', delete)
+        map('n', '<C-d>', delete)
+        map('i', '<C-t>', track)
+        map('n', '<C-t>', track)
 
         return true
     end
