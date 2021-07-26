e4f8061742b091bd2e31acff60df27e78c9bc557
jrasmusbm <jrasmusbm@gmail.com>
2021-07-26 22:19:46 +0200

    Configure on_attach to only run supported features
    **Why** is the change needed?
    
    My default bindings were not working in the repositories where I wanted
    them to.
    
    Closes #209

--- vim/lua/jrasmusbm/lsp/efm/init.lua
+++ vim/lua/jrasmusbm/lsp/efm/init.lua
@@ -3,18 +3,13 @@ local M = {}
 M.setup = function(options)
   require"lspconfig".efm.setup {
     on_attach = options.on_attach,
-    cmd = {
-      "efm-langserver", "-c",
-      vim.fn.stdpath("config") .. "/../efm-langserver/config.yaml"
-    },
-    init_options = {documentFormatting = true},
+    cmd = {"efm-langserver"},
     settings = {
       rootMarkers = {".git/"},
       languages = {
         bash = require("jrasmusbm.lsp.efm.bash"),
         css = require("jrasmusbm.lsp.efm.css"),
         html = require("jrasmusbm.lsp.efm.html"),
-        init = require("jrasmusbm.lsp.efm.init"),
         javascript = require("jrasmusbm.lsp.efm.javascript"),
         json = require("jrasmusbm.lsp.efm.json"),
         lua = require("jrasmusbm.lsp.efm.lua"),
