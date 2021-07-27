9d3d2f0a850921debbdbdb4a2a1f083f27ea7cbb
jrasmusbm <jrasmusbm@gmail.com>
2021-07-27 22:20:20 +0200

    Add treesitter queries for python

---
+++ vim/queries/python/textobjects.scm
@@ -0,0 +1,23 @@
+; Argument {{{
+; Inner {{{
+
+[
+  (argument_list
+    (_) @argument.inner
+  )
+  (parameters
+    (_) @argument.inner
+  )
+  (dictionary
+    (_) @argument.inner
+  )
+  (list
+    (_) @argument.inner
+  )
+  (pattern_list
+    (_) @argument.inner
+  )
+]
+
+; }}}
+; }}}
