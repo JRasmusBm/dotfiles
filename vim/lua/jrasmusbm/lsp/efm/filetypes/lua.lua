88bb30408be21aa71d9f5ae102a619dbefa5f631
jrasmusbm <jrasmusbm@gmail.com>
2021-09-04 13:21:42 +0200

    Add a lua-format config
    **Why** is the change needed?
    
    So that I can stand the format output by lua-format.

--- vim/lua/jrasmusbm/lsp/efm/filetypes/lua.lua
+++ vim/lua/jrasmusbm/lsp/efm/filetypes/lua.lua
@@ -1,3 +1,9 @@
+local Path = require("plenary.path")
+
 return {
-  {formatCommand = "lua-format -i", formatStdin = true}
+  {
+    formatCommand = "lua-format -c " .. Path:new("~/.lua-format.yaml"):expand() ..
+      " -i",
+    formatStdin = true,
+  },
 }
