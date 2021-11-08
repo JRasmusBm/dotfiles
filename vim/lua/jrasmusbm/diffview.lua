1f3f0e619727eba777c48c93c9af0e286aae56ea
jrasmusbm <jrasmusbm@gmail.com>
2021-11-08 19:29:03 +0100

    Fix issue with diffview changelog
    **Why** is the change needed?
    
    They introduced a minor breaking change.
    
    Closes #269

--- vim/lua/jrasmusbm/diffview.lua
+++ vim/lua/jrasmusbm/diffview.lua
@@ -1,15 +1,18 @@
-local cb = require'diffview.config'.diffview_callback
-require'diffview'.setup {
-    diff_binaries = false, -- Show diffs for binaries
-    file_panel = {width = 35, use_icons = true},
-    key_bindings = {
-        view = {["R"] = cb("refresh_files"), ["-"] = cb("toggle_stage_entry")},
-        file_panel = {
-            ["j"] = cb("next_entry"),
-            ["k"] = cb("prev_entry"),
-            ["<cr>"] = cb("select_entry"),
-            ["o"] = cb("select_entry"),
-            ["R"] = cb("refresh_files")
-        }
-    }
+local cb = require"diffview.config".diffview_callback
+
+require"diffview".setup {
+  diff_binaries = false, -- Show diffs for binaries
+  file_panel = {width = 35},
+  use_icons = true,
+  key_bindings = {
+    view = {["R"] = cb("refresh_files"), ["-"] = cb("toggle_stage_entry")},
+    file_panel = {
+      ["j"] = cb("next_entry"),
+      ["k"] = cb("prev_entry"),
+      ["<cr>"] = cb("select_entry"),
+      ["o"] = cb("select_entry"),
+      ["R"] = cb("refresh_files"),
+    },
+  },
 }
+
