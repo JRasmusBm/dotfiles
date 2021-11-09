7d706c92d6be82cad0e2aac0d4ff7afd17521d98
jrasmusbm <jrasmusbm@gmail.com>
2021-11-09 10:37:11 +0100

    Migrate stats config to lua
    Closes #292

--- vim/lua/jrasmusbm/plugin_config/stats.lua
+++ vim/lua/jrasmusbm/plugin_config/stats.lua
@@ -1,6 +1,8 @@
+local mappings = require("jrasmusbm.utils.mappings")
+
 vim.cmd [[
 packadd! vim-vmath
-
-nnoremap <leader>st  :call VMATH_Analyse()<CR>
-vnoremap <leader>st  y:call VMATH_Analyse()<CR>
 ]]
+
+mappings.nmap("<leader>st", ":call VMATH_Analyse()<CR>", {noremap = true})
+mappings.nmap("<leader>st", "y:call VMATH_Analyse()<CR>", {noremap = true})
