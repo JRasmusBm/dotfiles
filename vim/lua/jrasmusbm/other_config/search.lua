98e6f8cae2df2ed9ca3179c49cab7a70b74a8601
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 22:15:02 +0100

    Port search settings to lua

--- vim/lua/jrasmusbm/other_config/search.lua
+++ vim/lua/jrasmusbm/other_config/search.lua
@@ -1,9 +1,9 @@
-vim.cmd [[
-nnoremap <C-O> <C-O>zz
-nnoremap <C-I> <C-I>zz
+local mappings = require("jrasmusbm.utils.mappings")
 
-nnoremap n nzzzv
-nnoremap N Nzzzv
-vnoremap n nzzzv
-vnoremap N Nzzzv
-]]
+mappings.nmap("<C-O>", "<C-O>zz", {noremap = true})
+mappings.nmap("<C-I>", "<C-I>zz", {noremap = true})
+
+mappings.nmap("n", "nzzzv", {noremap = true})
+mappings.nmap("N", "Nzzzv", {noremap = true})
+mappings.vmap("n", "nzzzv", {noremap = true})
+mappings.vmap("N", "Nzzzv", {noremap = true})
