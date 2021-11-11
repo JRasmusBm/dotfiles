11face918283fd2d327bc8a490cd28f481848fb2
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 20:48:25 +0100

    Move python-specific plugin usage to ftplugin

--- vim/lua/jrasmusbm/plugin_config/python.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd [[
-autocmd Filetype ipynb nmap <silent><Leader>jb :VimpyterInsertPythonBlock<CR>
-autocmd Filetype ipynb nmap <silent><Leader>js :VimpyterStartJupyter<CR>
-autocmd Filetype ipynb nmap <silent><Leader>jn :VimpyterStartNteract<CR>
-]]
