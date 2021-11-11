122c86c831cef37bc855e1199e0209b9385b7b14
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 22:12:37 +0100

    Port tabs to lua

--- vim/lua/jrasmusbm/other_config/tabs.lua
+++ vim/lua/jrasmusbm/other_config/tabs.lua
@@ -1,6 +1,6 @@
-vim.cmd [[
-nnoremap ]t :tabn
-nnoremap [t :tabp
-nnoremap <leader>to :exec "tabe " . @%
-nnoremap <leader>tc :tabc
-]]
+local mappings = require("jrasmusbm.utils.mappings")
+
+mappings.nmap("]t", "<cmd>tabn", {noremap = true})
+mappings.nmap("[t", "<cmd>tabp", {noremap = true})
+mappings.nmap("<leader>to", "<cmd>exec \"tabe \" . @%<cr>", {noremap = true})
+mappings.nmap("<leader>tc", "<cmd>tabc", {noremap = true})
