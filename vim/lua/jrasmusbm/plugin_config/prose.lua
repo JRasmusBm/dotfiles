a412c07c1dcede13a0c233686ca609bc9029f2c9
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 20:53:12 +0100

    Port prose config to lua

--- vim/lua/jrasmusbm/plugin_config/prose.lua
+++ vim/lua/jrasmusbm/plugin_config/prose.lua
@@ -1,10 +1,15 @@
+local mappings = require("jrasmusbm.utils.mappings")
+
 vim.cmd [[
 packadd! thesaurus_query.vim
-
-let g:tq_map_keys=0
-let g:tq_use_vim_autocomplete=0
-
-inoremap <leader>cs b:ThesaurusQueryReplaceCurrentWord<CR>
-nnoremap <leader>cs :ThesaurusQueryReplaceCurrentWord<CR>
-vnoremap <leader>cs "ky:ThesaurusQueryReplace <C-r>k<CR>
 ]]
+
+vim.g.tq_map_keys = 0
+vim.g.tq_use_vim_autocomplete = 0
+
+mappings.imap("<leader>cs", "b:ThesaurusQueryReplaceCurrentWord<CR>",
+              {noremap = true})
+mappings.nmap("<leader>cs", ":ThesaurusQueryReplaceCurrentWord<CR>",
+              {noremap = true})
+mappings.vmap("<leader>cs", "\"ky:ThesaurusQueryReplace <C-r>k<CR>",
+              {noremap = true})
