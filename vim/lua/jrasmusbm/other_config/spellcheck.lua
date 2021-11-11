8dff3f2d8e7c0aa0a71565a79c5ad793322d605d
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 22:13:36 +0100

    Port spellcheck to lua

--- vim/lua/jrasmusbm/other_config/spellcheck.lua
+++ vim/lua/jrasmusbm/other_config/spellcheck.lua
@@ -1,8 +1,12 @@
-vim.cmd [[
-nnoremap <leader>ce :set spell spelllang+=en_us,<CR>
-nnoremap <leader>cv :set spell spelllang+=sv,<CR>
-nnoremap <leader>cd :set spell spelllang+=de,<CR>
-nnoremap <leader>cn :set spell spelllang+=nl,<CR>
-nnoremap <leader>ci :set invspell<CR>
-nnoremap <leader>cc :set spelllang=""<CR>
-]]
+local mappings = require("jrasmusbm.utils.mappings")
+
+mappings.nmap("<leader>ce", "<cmd>set spell spelllang+=en_us,<CR>",
+              {noremap = true})
+mappings.nmap("<leader>cv", "<cmd>set spell spelllang+=sv,<CR>",
+              {noremap = true})
+mappings.nmap("<leader>cd", "<cmd>set spell spelllang+=de,<CR>",
+              {noremap = true})
+mappings.nmap("<leader>cn", "<cmd>set spell spelllang+=nl,<CR>",
+              {noremap = true})
+mappings.nmap("<leader>ci", "<cmd>set invspell<CR>", {noremap = true})
+mappings.nmap("<leader>cc", "<cmd>set spelllang=\"\"<CR>", {noremap = true})
