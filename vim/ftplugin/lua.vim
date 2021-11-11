6a308c499c2ab3ada8ffef6a3eb93e76e26bc113
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 22:10:33 +0100

    Port window config to lua

--- vim/ftplugin/lua.vim
+++ vim/ftplugin/lua.vim
@@ -2,5 +2,5 @@ execute "source $DOTFILES/vim/snippets/lua.vim"
 
 nnoremap <buffer> <localleader>tf :luafile %
 
-nmap <buffer> <leader>me Imappings.f xys$)0f<ysE"f r,lys])"$i, {}
+nmap <buffer> <leader>me Imappings.f xys$)lysE"f r,lys])"$i, {}
 nmap <buffer> <leader>mr 0f.2l4x$F{a noremap=true,<Space>
--- vim/lua/jrasmusbm/other_config/windows.lua
+++ vim/lua/jrasmusbm/other_config/windows.lua
@@ -1,22 +1,22 @@
-vim.cmd [[
-set splitright
+local mappings = require("jrasmusbm.utils.mappings")
 
-nmap <leader>bd :bp! <BAR> bd #<CR>
+vim.opt.splitright = true
 
-nnoremap <C-j> <C-w>j
-nnoremap <C-k> <C-w>k
-nnoremap <C-h> <C-w>h
-nnoremap <C-l> <C-w>l
-nnoremap c<C-j> :bel sp %<cr>
-nnoremap c<C-k> :abo sp %<cr>
-nnoremap c<C-h> :lefta vsp %<cr>
-nnoremap c<C-l> :rightb vsp %<cr>
-nnoremap d<C-j> <C-w>j<C-w>c
-nnoremap d<C-k> <C-w>k<C-w>c
-nnoremap d<C-h> <C-w>h<C-w>c
-nnoremap d<C-l> <C-w>l<C-w>c
-nnoremap o<C-j> :bel sp %<cr><C-w>j<C-w>c
-nnoremap o<C-k> :abo sp %<cr><C-w>k<C-w>c
-nnoremap o<C-h> :lefta vsp %<cr><C-w>h<C-w>c
-nnoremap o<C-l> :rightb vsp %<cr><C-w>l<C-w>c
-]]
+mappings.nmap("<leader>bd", "<cmd>bp! <BAR> bd #<CR>", {})
+
+mappings.nmap("<C-j>", "<C-w>j", {noremap = true})
+mappings.nmap("<C-k>", "<C-w>k", {noremap = true})
+mappings.nmap("<C-h>", "<C-w>h", {noremap = true})
+mappings.nmap("<C-l>", "<C-w>l", {noremap = true})
+mappings.nmap("c<C-j>", "<cmd>bel sp %<cr>", {noremap = true})
+mappings.nmap("c<C-k>", "<cmd>abo sp %<cr>", {noremap = true})
+mappings.nmap("c<C-h>", "<cmd>lefta vsp %<cr>", {noremap = true})
+mappings.nmap("c<C-l>", "<cmd>rightb vsp %<cr>", {noremap = true})
+mappings.nmap("d<C-j>", "<C-w>j<C-w>c", {noremap = true})
+mappings.nmap("d<C-k>", "<C-w>k<C-w>c", {noremap = true})
+mappings.nmap("d<C-h>", "<C-w>h<C-w>c", {noremap = true})
+mappings.nmap("d<C-l>", "<C-w>l<C-w>c", {noremap = true})
+mappings.nmap("o<C-j>", "<cmd>bel sp %<cr><C-w>j<C-w>c", {noremap = true})
+mappings.nmap("o<C-k>", "<cmd>abo sp %<cr><C-w>k<C-w>c", {noremap = true})
+mappings.nmap("o<C-h>", "<cmd>lefta vsp %<cr><C-w>h<C-w>c", {noremap = true})
+mappings.nmap("o<C-l>", "<cmd>rightb vsp %<cr><C-w>l<C-w>c", {noremap = true})
