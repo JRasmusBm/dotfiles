28b0c211e013ee707b3ec6553a77fd8486b88f76
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 12:57:16 +0100

    Port snippets config to lua

--- vim/lua/jrasmusbm/plugin_config/snippets.lua
+++ vim/lua/jrasmusbm/plugin_config/snippets.lua
@@ -1,22 +1,27 @@
+local mappings = require("jrasmusbm.utils.mappings")
+
 vim.cmd [[
 packadd! vim-bushels
-
-inoremap <leader><leader> <++>
-
-imap <leader>dd <Plug>BushelsDeleteMarkerRow
-nmap <leader>dd <Plug>BushelsDeleteMarkerRow
-
-imap <leader>dm <Plug>BushelsDeleteMarker
-nmap <leader>dm <Plug>BushelsDeleteMarker
-
-imap <leader>da <Plug>BushelsDeleteAllMarkers
-nmap <leader>da <Plug>BushelsDeleteAllMarkers
-
-imap <leader>G <Plug>BushelsBackwardMarker
-nmap <leader>G <Plug>BushelsBackwardMarker
-
-imap <leader>g <Plug>BushelsForwardMarker
-nmap <leader>g <Plug>BushelsForwardMarker
-
-inoremap <leader>cp :call bushels#forward_marker(1)<Enter><C-R>.<Esc>:call bushels#forward_marker(1)
 ]]
+
+mappings.imap("<leader><leader>", "<++>", {noremap = true})
+
+mappings.imap("<leader>dd", "<Plug>BushelsDeleteMarkerRow")
+mappings.nmap("<leader>dd", "<Plug>BushelsDeleteMarkerRow")
+
+mappings.imap("<leader>dm", "<Plug>BushelsDeleteMarker")
+mappings.nmap("<leader>dm", "<Plug>BushelsDeleteMarker")
+
+mappings.imap("<leader>da", "<Plug>BushelsDeleteAllMarkers")
+mappings.nmap("<leader>da", "<Plug>BushelsDeleteAllMarkers")
+
+mappings.imap("<leader>G", "<Plug>BushelsBackwardMarker")
+mappings.nmap("<leader>G", "<Plug>BushelsBackwardMarker")
+
+mappings.imap("<leader>g", "<Plug>BushelsForwardMarker")
+mappings.nmap("<leader>g", "<Plug>BushelsForwardMarker")
+
+
+mappings.imap("<leader>cp",
+              "<cmd>call bushels#forward_marker(1)<Enter><C-R>.<Esc><cmd>call bushels#forward_marker(1)<cr>",
+              {noremap = true})
