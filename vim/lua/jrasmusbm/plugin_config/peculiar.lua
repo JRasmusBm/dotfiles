3a2cdc2f4ab4b0dff43f6271d973b3c4c85c4183
jrasmusbm <jrasmusbm@gmail.com>
2021-11-11 20:56:14 +0100

    Port peculiar config to lua

--- vim/lua/jrasmusbm/plugin_config/peculiar.lua
+++ vim/lua/jrasmusbm/plugin_config/peculiar.lua
@@ -1,10 +1,12 @@
+local mappings = require("jrasmusbm.utils.mappings")
+
 vim.cmd [[
 packadd! vim-peculiar
-
-nmap <localleader>v <Plug>PeculiarV
-nmap <localleader>g <Plug>PeculiarG
-nmap <localleader>n <Plug>PeculiarN
-nmap <localleader>r <Plug>PeculiarR
-
-let g:peculiar#suppress_highlight_n = 1
 ]]
+
+mappings.nmap("<localleader>v", "<Plug>PeculiarV")
+mappings.nmap("<localleader>g", "<Plug>PeculiarG")
+mappings.nmap("<localleader>n", "<Plug>PeculiarN")
+mappings.nmap("<localleader>r", "<Plug>PeculiarR")
+
+vim.g["peculiar#suppress_highlight_n"] = 1
