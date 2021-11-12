edb05f83be28e9747219fd1cf881f37195c78158
jrasmusbm <jrasmusbm@gmail.com>
2021-11-12 08:25:19 +0100

    Start handling large files gracefully
    **Why** is the change needed?
    
    There is more work needed here but I'll merge it anyways since I need it
    in my daily workflow.
    
    Closes #267

---
+++ vim/lua/jrasmusbm/other_config/large_files.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+local options = require("jrasmusbm.utils.options")
+
+vim.cmd [[
+augroup LargeFile
+ autocmd!
+ autocmd BufReadPre * lua require("jrasmusbm.other_config.large_files").handle_large_file()
+ autocmd BufReadPost * lua require("jrasmusbm.other_config.large_files").clean_up_large_file()
+ augroup END
+]]
+
+M.large_file_limit = 3000
+
+local function is_large_file()
+  return M.large_file_limit < vim.api.nvim_buf_line_count(0)
+end
+
+M.handle_buf_enter = function() end
+
+M.handle_buf_leave = function() end
+
+M.handle_large_file = function()
+  if not is_large_file() then return end
+
+  vim.bo.eventignore = options.list {"FileType"}
+
+  vim.cmd [[
+   augroup LargeFileEnter
+     autocmd LargeFile BufEnter <buffer> lua require("jrasmusbm.other_config.large_files").handle_buf_enter()
+     autocmd LargeFile BufLeave <buffer> lua require("jrasmusbm.other_config.large_files").handle_buf_leave()
+   augroup END
+  ]]
+end
+
+M.clean_up_large_file = function() if not is_large_file() then return end end
+
+return M
