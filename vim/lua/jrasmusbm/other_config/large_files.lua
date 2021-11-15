2878cff28f7a076c2ab3ff472bbdbe06c0ff61d6
jrasmusbm <jrasmusbm@gmail.com>
2021-11-15 22:33:52 +0100

    Fix autocommands in other_config.large_files
    **Why** is the change needed?
    
    They were not being run correctly. Next step will be to start adding
    optimizations.
    
    Closes #267

--- vim/lua/jrasmusbm/other_config/large_files.lua
+++ vim/lua/jrasmusbm/other_config/large_files.lua
@@ -1,39 +1,51 @@
 local M = {}
 
 local options = require("jrasmusbm.utils.options")
+local scroll = require("jrasmusbm.plugin_config.scroll")
 
 vim.cmd [[
 augroup LargeFile
  autocmd!
- autocmd BufReadPre * lua require("jrasmusbm.other_config.large_files").handle_large_file()
- autocmd BufReadPost * lua require("jrasmusbm.other_config.large_files").clean_up_large_file()
+ autocmd BufReadPre * lua require("jrasmusbm.other_config.large_files").buf_read_pre(vim.fn.expand("<afile>"))
+ autocmd BufReadPost * lua require("jrasmusbm.other_config.large_files").buf_read_post()
  augroup END
 ]]
+local previous_settings = {}
 
-M.large_file_limit = 3000
+local estimated_bytes_per_line = 50
+local max_lines = 2000
+M.large_file_limit_bytes = max_lines * estimated_bytes_per_line
 
-local function is_large_file()
-  return false
-  -- return M.large_file_limit < vim.api.nvim_buf_line_count(0)
+local function is_large_file(file_path)
+  return M.large_file_limit_bytes < vim.fn.getfsize(file_path)
 end
 
-M.handle_buf_enter = function() end
+M.buf_enter = function()
+  scroll.reset_mappings()
+end
 
-M.handle_buf_leave = function() end
+M.buf_leave = function()
+  scroll.declare_mappings()
+end
 
-M.handle_large_file = function()
-  if not is_large_file() then return end
+M.buf_read_pre = function(file_path)
+  if not is_large_file(file_path) then return end
 
-  vim.bo.eventignore = options.list {"FileType"}
+  previous_settings.eventignore = vim.o.eventignore
+  vim.o.eventignore = options.list {"FileType"}
 
   vim.cmd [[
    augroup LargeFileEnter
-     autocmd LargeFile BufEnter <buffer> lua require("jrasmusbm.other_config.large_files").handle_buf_enter()
-     autocmd LargeFile BufLeave <buffer> lua require("jrasmusbm.other_config.large_files").handle_buf_leave()
+     autocmd BufEnter <buffer> lua require("jrasmusbm.other_config.large_files").buf_enter()
+     autocmd BufLeave <buffer> lua require("jrasmusbm.other_config.large_files").buf_leave()
    augroup END
   ]]
 end
 
-M.clean_up_large_file = function() if not is_large_file() then return end end
+M.buf_read_post = function()
+  if previous_settings.eventignore ~= nil then
+    vim.o.eventignore = previous_settings.eventignore
+  end
+end
 
 return M
--- vim/lua/jrasmusbm/plugin_config/scroll.lua
+++ vim/lua/jrasmusbm/plugin_config/scroll.lua
@@ -1,8 +1,25 @@
+local mappings = require("jrasmusbm.utils.mappings")
+
 vim.cmd [[
 packadd! vim-smoothie
 ]]
 
+vim.g.smoothie_no_default_mappings = true
 vim.g.smoothie_update_interval = 20
 vim.g.smoothie_speed_constant_factor = 20
 vim.g.smoothie_speed_linear_factor = 20
 vim.g.smoothie_speed_exponentiation_factor = 0.95
+
+local M = {}
+
+M.declare_mappings = function()
+  mappings.nmap("<C-D>", "<Plug>(SmoothieDownwards)")
+  mappings.nmap("<C-U>", "<Plug>(SmoothieUpwards)")
+end
+
+M.reset_mappings = function()
+  mappings.nmap("<C-D>", "<C-D>", {noremap = true})
+  mappings.nmap("<C-U>", "<C-U>", {noremap = true})
+end
+
+return M
