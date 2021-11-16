12f886da88a30505cec59307cb882a0227dd4c55
jrasmusbm <jrasmusbm@gmail.com>
2021-11-16 09:01:20 +0100

    Turn off matchparen for large files
    Closes #267

--- vim/lua/jrasmusbm/other_config/large_files.lua
+++ vim/lua/jrasmusbm/other_config/large_files.lua
@@ -6,8 +6,8 @@ local scroll = require("jrasmusbm.plugin_config.scroll")
 vim.cmd [[
 augroup LargeFile
  autocmd!
- autocmd BufReadPre * lua require("jrasmusbm.other_config.large_files").buf_read_pre(vim.fn.expand("<afile>"))
- autocmd BufReadPost * lua require("jrasmusbm.other_config.large_files").buf_read_post()
+ autocmd BufReadPre * lua require("jrasmusbm.other_config.large_files").buf_read_pre(vim.fn.expand("<afile>:p"))
+ autocmd BufReadPost * lua require("jrasmusbm.other_config.large_files").buf_read_post(vim.fn.expand("<afile:p>"))
  augroup END
 ]]
 local previous_settings = {}
@@ -17,15 +17,24 @@ local max_lines = 2000
 M.large_file_limit_bytes = max_lines * estimated_bytes_per_line
 
 local function is_large_file(file_path)
-  return M.large_file_limit_bytes < vim.fn.getfsize(file_path)
+  local size_in_bytes = vim.fn.getfsize(file_path)
+  return M.large_file_limit_bytes < size_in_bytes
 end
 
 M.buf_enter = function()
   scroll.reset_mappings()
+  vim.cmd [[
+  NoMatchParen
+  IlluminationDisable
+  ]]
 end
 
 M.buf_leave = function()
   scroll.declare_mappings()
+  vim.cmd [[
+  DoMatchParen
+  IlluminationEnable
+  ]]
 end
 
 M.buf_read_pre = function(file_path)
@@ -34,12 +43,12 @@ M.buf_read_pre = function(file_path)
   previous_settings.eventignore = vim.o.eventignore
   vim.o.eventignore = options.list {"FileType"}
 
-  vim.cmd [[
-   augroup LargeFileEnter
-     autocmd BufEnter <buffer> lua require("jrasmusbm.other_config.large_files").buf_enter()
-     autocmd BufLeave <buffer> lua require("jrasmusbm.other_config.large_files").buf_leave()
+  local augroup_name= "LargeFileEnter" ..vim.fn.substitute(file_path, "\\A", "", "g")
+  vim.cmd ("augroup " .. augroup_name .. [[
+     autocmd BufEnter <buffer> lua require("jrasmusbm.other_config.large_files").buf_enter(vim.fn.expand("<afile:p>"))
+     autocmd BufLeave <buffer> lua require("jrasmusbm.other_config.large_files").buf_leave(vim.fn.expand("<afile:p>"))
    augroup END
-  ]]
+   ]])
 end
 
 M.buf_read_post = function()
@@ -47,5 +56,7 @@ M.buf_read_post = function()
     vim.o.eventignore = previous_settings.eventignore
   end
 end
+
+M.buf_read_pre(vim.fn.expand("%:p"))
 
 return M
