384cb0ab38cb991e099d7aa516c7db1481f7c1eb
jrasmusbm <jrasmusbm@gmail.com>
2021-11-17 06:35:23 +0100

    Add reason on cmp.mapping.complete
    **Why** is the change needed?
    
    [nvim-cmp #531 - cmp.mapping.complete does not take context into account](https://github.com/hrsh7th/nvim-cmp/issues/531)
    
    Closes #237

--- vim/lua/jrasmusbm/lsp/completion.lua
+++ vim/lua/jrasmusbm/lsp/completion.lua
@@ -4,38 +4,42 @@ M.setup = vim.schedule_wrap(function()
   local cmp = require("cmp")
 
   cmp.setup({
-      snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body)
-        end,
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
+    },
+    mapping = {
+      ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+      ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
+      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete({
+        reason = cmp.ContextReason.Auto,
+      }), {"i", "c"}),
+      ["<M-Space>"] = cmp.mapping(cmp.mapping.complete({
+        reason = cmp.ContextReason.Manual,
+      }), {"i", "c"}),
+      ["<Enter>"] = cmp.mapping(cmp.mapping.confirm(), {"i", "c"}),
+      ["<C-e>"] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
+    },
+    sources = cmp.config.sources({
+      {name = "nvim_lsp", priority = 1},
+      {
+        name = "buffer",
+        get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+        priority = 2,
       },
-      mapping = {
-        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-        ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
-        ["<Enter>"] = cmp.mapping(cmp.mapping.confirm(), {"i", "c"}),
-        ["<C-e>"] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
-      },
-      sources = cmp.config.sources({
-          {name = "nvim_lsp", priority=1},
-          {
-            name = "buffer",
-            get_bufnrs = function() return vim.api.nvim_list_bufs() end,
-            priority=2,
-          },
-          {name = "path", priority=3},
-        }),
-    })
+      {name = "path", priority = 3},
+    }),
+  })
 
   cmp.setup.cmdline("/", {sources = {{name = "buffer"}}})
 
   cmp.setup.cmdline(":", {
-      sources = cmp.config.sources({{name = "path"}, {name = "cmdline"}}),
-    })
-  end)
+    sources = cmp.config.sources({{name = "path"}, {name = "cmdline"}}),
+  })
+end)
 
-  M.capabilities = vim.lsp.protocol.make_client_capabilities()
-  M.capabilities = require("cmp_nvim_lsp").update_capabilities(M.capabilities)
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities = require("cmp_nvim_lsp").update_capabilities(M.capabilities)
 
-  return M
+
+return M
