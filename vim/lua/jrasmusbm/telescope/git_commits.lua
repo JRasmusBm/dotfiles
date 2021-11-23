6d98fc5097e18b656cbb891b9f5d16a85617f2b8
jrasmusbm <jrasmusbm@gmail.com>
2021-11-23 10:30:21 +0100

    Add reset hard to git_commits

--- vim/lua/jrasmusbm/telescope/git_commits.lua
+++ vim/lua/jrasmusbm/telescope/git_commits.lua
@@ -1,10 +1,18 @@
 local M = {}
 
 local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
 
 M.git_commits = function(options_)
     local options = options_ or {}
-    options["attach_mappings"] = function(_, map)
+    options["attach_mappings"] = function(prompt_bufnr, map)
+        local reset_hard = function()
+            local selection = action_state.get_selected_entry()
+            vim.api.nvim_command('Git rh ' .. selection.value)
+            actions.close(prompt_bufnr)
+            vim.api.nvim_command('call jrasmusbm#git#list_tree()')
+        end
+
         local rebase_interactive = function()
             local selection = action_state.get_selected_entry()
             vim.api.nvim_command('G rbi ' .. selection.value)
@@ -15,6 +23,9 @@ M.git_commits = function(options_)
 
             vim.api.nvim_command('Gbrowse ' .. selection.value)
         end
+
+        map('i', '<C-h>', reset_hard)
+        map('n', '<C-h>', reset_hard)
 
         map('i', '<C-b>', browse)
         map('n', '<C-b>', browse)
