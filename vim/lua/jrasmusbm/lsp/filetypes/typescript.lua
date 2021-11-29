03c8be42c0b83f4b6c5d43dac379416f68dbbe00
jrasmusbm <jrasmusbm@gmail.com>
2021-11-29 08:51:55 +0100

    Support tsserver in javascript

--- vim/lua/jrasmusbm/lsp/filetypes/typescript.lua
+++ vim/lua/jrasmusbm/lsp/filetypes/typescript.lua
@@ -1,13 +1,17 @@
 local M = {}
 
 function M.setup(options)
+  local filetypes = require("jrasmusbm.filetypes").typescript
+  for _, filetype in ipairs(require("jrasmusbm.filetypes").javascript) do
+    table.insert(filetypes, filetype)
+  end
   require("lspconfig").tsserver.setup {
     capabilities = options.capabilities,
     on_attach = function(client)
       client.resolved_capabilities.document_formatting = false
       options.on_attach(client)
     end,
-    filetypes = {"typescript", "typescript.express", "typescript.tsx"},
+    filetypes = filetypes,
   }
 end
 
