faa294bb036dad3251a3cf81cd7cb4fd1dc8c9c8
jrasmusbm <jrasmusbm@gmail.com>
2021-12-10 22:53:46 +0100

    Use xdg-open as netrw_gx
    **Why** is the change needed?
    
    netrw_gx is amazing, it lets you choose an application to open the given
    file / protocol with. I feel like it's good to rely on xdg-open, but
    maybe I'll use my own open script one day.

--- vim/lua/jrasmusbm/plugin_config/netrw.lua
+++ vim/lua/jrasmusbm/plugin_config/netrw.lua
@@ -3,6 +3,6 @@ local mappings = require("jrasmusbm.utils.mappings")
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 vim.g.netrw_menu = 0
 vim.g.netrw_banner = 0
-vim.g.netrw_http_cmd = "br"
+vim.g.netrw_http_cmd = "xgd-open"
 
 mappings.nmap("<Leader>d", ":Ex<CR>", {noremap = true})
