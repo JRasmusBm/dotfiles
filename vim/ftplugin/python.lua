2ccb0ac1e3680c0aeba6499042e054d4a8beb229
jrasmusbm <jrasmusbm@gmail.com>
2022-02-11 09:37:28 +0100

    refactor: Remove RunFile
    **Why** is the change needed?
    
    I prefer using VTR directly

--- vim/ftplugin/python.lua
+++ vim/ftplugin/python.lua
@@ -1,14 +1,6 @@
 vim.cmd [[
 match BadWhitespace /\s\+$/
 
-function! RunFile() abort
-  w
-  let path = shellescape(@%, 1)
-  let module_path = join(split(split(path, '\.')[0], '/') , '.') . "'"
-  echo module_path
-  exec 'VtrSendCommandToRunner python -m ' . module_path
-endfunction
-
 execute "source $DOTFILES/vim/snippets/python.vim"
 ]]
 
