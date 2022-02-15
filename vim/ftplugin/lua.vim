3f392e96b089939158f66f77e5f77f8de300a0e6
jrasmusbm <jrasmusbm@gmail.com>
2022-02-15 22:59:50 +0100

    feat: Move lua snippets over to luasnip

--- vim/ftplugin/lua.vim
+++
@@ -1,6 +0,0 @@
-execute "source $DOTFILES/vim/snippets/lua.vim"
-
-nnoremap <buffer> <localleader>tf :luafile %
-
-nmap <buffer> <leader>me Imappings.f xys$)lysE"f r,lys])"$i, {}
-nmap <buffer> <leader>mr 0f.2l4x$F{a noremap=true,<Space>
--- vim/lua/jrasmusbm/snippets/init.lua
+++ vim/lua/jrasmusbm/snippets/init.lua
@@ -1,3 +1,11 @@
-local luasnip = require("luasnip")
+vim.cmd [[
+packadd! LuaSnip
+]]
 
+local ls = require("luasnip")
 
+vim.keymap.set({"i", "s", "n"}, "<leader>s", function()
+  if ls.expand_or_jumpable() then ls.expand_or_jump() end
+end, {silent = true})
+
+ls.config.set_config {history = true, updateevents = "TextChanged,TextChangedI"}
