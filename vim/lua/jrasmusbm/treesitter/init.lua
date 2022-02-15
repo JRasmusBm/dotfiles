3f7274a42c361f218e3293e48fd19927551ff2ec
jrasmusbm <jrasmusbm@gmail.com>
2022-02-15 21:36:32 +0100

    feat: Disable markdown parsing

--- vim/lua/jrasmusbm/treesitter/init.lua
+++ vim/lua/jrasmusbm/treesitter/init.lua
@@ -1,13 +1,13 @@
 require("jrasmusbm.treesitter.parsers")
 
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained",
-    highlight = {enable = true},
-    playground = require("jrasmusbm.treesitter.playground").playground,
-    query_linter = require("jrasmusbm.treesitter.query_linter").query_linter,
-    textobjects = {
-        select = require("jrasmusbm.treesitter.select").select,
-        swap = require("jrasmusbm.treesitter.swap").swap,
-        move = require("jrasmusbm.treesitter.move").move
-    }
+require"nvim-treesitter.configs".setup {
+  ensure_installed = "maintained",
+  highlight = {enable = true, disable = {"markdown"}},
+  playground = require("jrasmusbm.treesitter.playground").playground,
+  query_linter = require("jrasmusbm.treesitter.query_linter").query_linter,
+  textobjects = {
+    select = require("jrasmusbm.treesitter.select").select,
+    swap = require("jrasmusbm.treesitter.swap").swap,
+    move = require("jrasmusbm.treesitter.move").move,
+  },
 }
