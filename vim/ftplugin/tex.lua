ea646b87404fe0d98a21501ce62ae832dd751ffe
jrasmusbm <jrasmusbm@gmail.com>
2022-02-26 22:04:24 +0100

    feat(snippets): start migrating from luasnip

---
+++ vim/ftplugin/tex.lua
@@ -0,0 +1,14 @@
+vim.cmd [[
+match BadWhitespace /\s\+$/
+]]
+
+vim.opt.textwidth = 60
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
+
+local ls = require("luasnip")
+local ls_utils = require("jrasmusbm.snippets.utils.init")
+
+ls.add_snippets("tex", {})
+
+ls_utils.load_shared({filetype = "tex", shared = "latex"})
