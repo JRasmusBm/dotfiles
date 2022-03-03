b5c2c5016782d03eb02370fce5a1c430ecd98a3b
jrasmusbm <jrasmusbm@gmail.com>
2022-03-03 16:46:25 +0100

    fix: Make finish_file work with last line
    Closes #365

--- vim/ftplugin/gitcommit.lua
+++ vim/ftplugin/gitcommit.lua
@@ -1,5 +1,8 @@
+vim.cmd[[
 set textwidth=72
 setlocal spell spelllang=en_us
 
 execute "source $DOTFILES/vim/snippets/markdown.vim"
-execute "source $DOTFILES/vim/snippets/gitcommit.vim"
+]]
+
+vim.keymap.set({ "i" }, "<Leader>c", require("jrasmusbm.mappings").finish_file)
