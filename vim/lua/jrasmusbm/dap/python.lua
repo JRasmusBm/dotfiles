b275ffb97ea241769d70ec3ca3f6ae2da8e3eaba
jrasmusbm <jrasmusbm@gmail.com>
2022-03-14 09:18:40 +0100

    refactor: Remove unused code

--- vim/lua/jrasmusbm/dap/python.lua
+++ vim/lua/jrasmusbm/dap/python.lua
@@ -35,26 +35,4 @@ dap.configurations.python = {
   },
 }
 
-M.debug_test = function(cmd)
-  local temp_unittest = vim.g["test#python#pyunit#executable"]
-  local temp_pytest = vim.g["test#python#pytest#executable"]
-
-  vim.g["test#python#pytest#executable"] = "python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m pytest"
-  vim.g["test#python#pyunit#executable"] = "python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m unittest"
-
-  vim.cmd(cmd)
-
-  vim.g["test#python#pytest#executable"] = temp_pytest
-  vim.g["test#python#pyunit#executable"] = temp_unittest
-
-  vim.defer_fn(
-    vim.schedule_wrap(function()
-      vim.cmd [[
-  Debugpy attach 0.0.0.0 5678
-  ]]
-    end),
-    1000
-  )
-end
-
 return M
