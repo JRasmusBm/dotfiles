626eef548d103efeca6a56139b72022b6a573ce5
jrasmusbm <jrasmusbm@gmail.com>
2022-03-16 20:19:13 +0100

    fix: Only queue DiffviewOpen
    **Why** is the change needed?
    
    Otherwise it always errors out, since it requires an argument to work.

--- vim/ftplugin/fugitive.lua
+++ vim/ftplugin/fugitive.lua
@@ -1,7 +1,5 @@
 vim.keymap.set({ "n" }, "D", function()
-  vim.cmd [[
-  DiffViewOpen
-  ]]
+  vim.fn.feedkeys(":DiffviewOpen ")
 end, { buffer = 0 })
 
 vim.keymap.set({ "n" }, "L", vim.fn["jrasmusbm#git#open_tree"], { silent = true })
