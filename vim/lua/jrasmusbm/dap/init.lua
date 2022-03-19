4fef41f7687509b49367bbd1cebc0443691c9789
jrasmusbm <jrasmusbm@gmail.com>
2022-03-19 12:43:33 +0100

    feat: configure nvim-dap-virtual-text

--- vim/lua/jrasmusbm/dap/init.lua
+++ vim/lua/jrasmusbm/dap/init.lua
@@ -7,7 +7,11 @@ packadd! debugpy.nvim
 ]]
 require "jrasmusbm.dap.python"
 
-require("nvim-dap-virtual-text").setup()
+require("nvim-dap-virtual-text").setup {
+  all_frames = true,
+  highlight_changed_variables = false,
+}
+
 require("dapui").setup {
   sidebar = {
     elements = {
