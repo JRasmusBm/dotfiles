a0c8a198c4680227c09372e27c1d38df4b6f3519
jrasmusbm <jrasmusbm@gmail.com>
2022-03-19 12:31:46 +0100

    fix: add correct treesitter parsers
    **Why** is the change needed?
    
    So that the nvim-dap-virtual-text shows up (it uses treesitter under the
    hood).
    
    Closes #350

--- vim/lua/jrasmusbm/treesitter/init.lua
+++ vim/lua/jrasmusbm/treesitter/init.lua
@@ -1,4 +1,12 @@
-require "jrasmusbm.treesitter.parsers"
+local filetype_to_parsername = require("nvim-treesitter.parsers").filetype_to_parsername
+
+for target, sources in pairs(require "jrasmusbm.filetypes") do
+  for _, source in ipairs(sources) do
+    if source ~= target then
+      filetype_to_parsername[source] = target
+    end
+  end
+end
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = "maintained",
--- vim/lua/jrasmusbm/treesitter/parsers/typescript.lua
+++
@@ -1,9 +0,0 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-local filetype_to_parsername = require("nvim-treesitter.parsers").filetype_to_parsername
-
-for _, filetype in ipairs(require("jrasmusbm.filetypes").typescript) do
-  if filetype ~= "typescript" then
-    filetype_to_parsername["typescript"] = filetype
-  end
-end
