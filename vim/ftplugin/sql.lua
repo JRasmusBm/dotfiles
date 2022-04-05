2824e4fb6a455102e107db3556ad5f8ce457cdd9
jrasmusbm <jrasmusbm@gmail.com>
2022-04-05 08:58:55 +0200

    feat(sql): start adding snippets

--- vim/ftplugin/sql.lua
+++ vim/ftplugin/sql.lua
@@ -15,3 +15,29 @@ vim.keymap.set({ "n" }, ";c", "", {})
 vim.g.VtrStripLeadingWhitespace = 0
 
 vim.opt.formatprg = "sqlformat - -k upper -r --indent_columns --wrap_after 60"
+
+local ls = require "luasnip"
+local s = ls.s
+local fmt = require("luasnip.extras.fmt").fmt
+local ls_utils = require "jrasmusbm.snippets.utils.init"
+local i = ls.insert_node
+local d = ls.dynamic_node
+local t = ls.text_node
+local rep = require("luasnip.extras").rep
+
+ls.add_snippets("sql", {
+  s(
+    { trig = "running", name = "get running queries" },
+    fmt(
+      [[
+SELECT pid, now() - backend_start, query
+FROM pg_stat_activity
+WHERE state = 'active'
+  AND pid != pg_backend_pid()
+  AND query !~~ 'START_REPLICATION%'
+  ORDER B Y backend_start;
+  ]],
+      {}
+    )
+  ),
+})
