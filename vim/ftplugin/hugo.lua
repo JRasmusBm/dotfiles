6beaf9c24e7ba3b2aec4bee32db6be808ff741e9
jrasmusbm <jrasmusbm@gmail.com>
2022-04-16 13:05:00 +0200

    feat(hugo): add ftplugin

---
+++ vim/ftplugin/hugo.lua
@@ -0,0 +1,23 @@
+local ls = require "luasnip"
+local s = ls.s
+local fmt = require("luasnip.extras.fmt").fmt
+local ls_utils = require "jrasmusbm.snippets.utils.init"
+local i = ls.insert_node
+local d = ls.dynamic_node
+local t = ls.text_node
+local rep = require("luasnip.extras").rep
+
+ls.add_snippets("hugo", {
+  s(
+    { trig = "ht", name = "hugo tag" },
+    fmt(
+      [[
+{{{{ {} }}}}
+{}
+{{{{ end }}}}
+{}
+  ]],
+      { i(1), i(2), i(0) }
+    )
+  ),
+})
