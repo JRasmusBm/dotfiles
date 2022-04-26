0a497239653a7e851cdba7b3e29bcf5bc5872055
jrasmusbm <jrasmusbm@gmail.com>
2022-04-26 21:04:41 +0200

    style: remove unused variables

--- vim/ftplugin/hugo.lua
+++ vim/ftplugin/hugo.lua
@@ -1,11 +1,7 @@
 local ls = require "luasnip"
 local s = ls.s
 local fmt = require("luasnip.extras.fmt").fmt
-local ls_utils = require "jrasmusbm.snippets.utils.init"
 local i = ls.insert_node
-local d = ls.dynamic_node
-local t = ls.text_node
-local rep = require("luasnip.extras").rep
 
 ls.add_snippets("hugo", {
   s(
