555a9b6524a110c76d1037badddaa618fd2676b6
jrasmusbm <jrasmusbm@gmail.com>
2022-05-20 22:29:48 +0200

    fix: use built-in method for checking capabilities
    Closes #411
    
    **Why** is the change needed?
    
    They were renamed which broke a lot of stuff. Hoping using official
    functions will protect against this going forward.

--- vim/lua/jrasmusbm/lsp/attach.lua
+++ vim/lua/jrasmusbm/lsp/attach.lua
@@ -114,19 +114,19 @@ function M.on_attach(client, bufnr)
     { severity_sort = true }
   )
 
-  -- -- Set autocommands conditional on server_capabilities
-  -- if capabilities.document_highlight then
-  --   vim.api.nvim_exec(
-  --     [[
-  --     augroup lsp_document_highlight
-  --     autocmd! * <buffer>
-  --     autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --     autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --     ]],
-  --     false
-  --   )
-  -- end
+  -- Set autocommands conditional on server_capabilities
+  if client.supports_method "textDocument/documentHighlight" then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+      autocmd! * <buffer>
+      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+      ]],
+      false
+    )
+  end
 end
 
 return M
