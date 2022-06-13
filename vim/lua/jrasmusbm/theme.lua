f36ccc5b463fdb9dd4e0d9f87a721e43e4ff70c9
jrasmusbm <JRasmusBm@gmail.com>
2022-06-13 07:44:22 +0200

    fix(theme): default to dark in vim

--- vim/lua/jrasmusbm/theme.lua
+++ vim/lua/jrasmusbm/theme.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 M.get_current_theme = function()
-  io.input(vim.fn.getenv "HOME" .. "/.config/jrasmusbm/theme")
+  local filename =vim.fn.getenv "HOME" .. "/.config/jrasmusbm/theme"
+  if io.open(filename)==nil then
+   return 'dark' 
+  end
+
+  io.input(filename)
   local theme = io.read()
   io.close()
   return theme
