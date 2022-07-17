5aa2c1f667fc36b68606c6c5002259abc503bb4a
jrasmusbm <JRasmusBm@gmail.com>
2022-07-17 17:29:08 +0200

    fix(yank): disable on termux
    **Why** is the change needed?
    
    I want to be able to scroll there

--- vim/plugin/yanking.lua
+++ vim/plugin/yanking.lua
@@ -1,9 +1,11 @@
 local mappings = require "jrasmusbm.utils.mappings"
 
-mappings.nmap("<C-Y>", '"+y', { noremap = true })
-mappings.nmap("<C-P>", '"+p', { noremap = true })
-mappings.vmap("<C-Y>", '"+y', { noremap = true })
-mappings.vmap("<C-P>", '"+p', { noremap = true })
+if not require("jrasmusbm.compat").in_termux() then
+  mappings.nmap("<C-Y>", '"+y', { noremap = true })
+  mappings.nmap("<C-P>", '"+p', { noremap = true })
+  mappings.vmap("<C-Y>", '"+y', { noremap = true })
+  mappings.vmap("<C-P>", '"+p', { noremap = true })
+end
 
 vim.cmd [[
 augroup ShowYank
