77132f8f230bce770d4525a711b5c2fe2f144001
jrasmusbm <jrasmusbm@gmail.com>
2022-09-01 09:09:50 +0200

    refactor(scratchpad): migrate Frc to Lua

---
+++ vim/ftplugin/scratchpad.lua
@@ -0,0 +1,13 @@
+vim.cmd [[
+inoremap <buffer> <leader>c :call jrasmusbm#scratchpad#exit()
+nnoremap <buffer> <leader>C :call jrasmusbm#scratchpad#exit()
+
+setlocal textwidth=0
+setlocal wrap
+
+set spell
+set spelllang=en_us,
+set spelllang+=sv,
+set spelllang+=de,
+set spelllang+=nl,
+]]
