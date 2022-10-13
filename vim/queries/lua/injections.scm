661c361a57d51dc01f10a57ada6668f70dc1b1b9
jrasmusbm <jrasmusbm@gmail.com>
2022-10-13 21:54:19 +0200

    feat(lua): highlight vim strings correctly
    Closes: #476

---
+++ vim/queries/lua/injections.scm
@@ -0,0 +1,7 @@
+(function_call
+  name: (dot_index_expression
+    table: (identifier) @_table (#eq? @_table "vim")
+    field: (identifier) @_field (#eq? @_field "cmd")
+  )
+  arguments: (arguments (string) @vim)
+)
