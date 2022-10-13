9b8c7bd033712a8f3d64473379e8bb71b01f596c
jrasmusbm <jrasmusbm@gmail.com>
2022-10-13 22:20:33 +0200

    feat(lua): mark some statements

---
+++ vim/queries/lua/textobjects.scm
@@ -0,0 +1,21 @@
+  (parameters
+    (_) @argument.inner
+  )
+
+  (table_constructor
+    (_) @argument.inner
+  )
+
+  (arguments
+    (_) @argument.inner
+  )
+
+[
+ (variable_declaration)
+ (function_declaration)
+ (assignment_statement)
+ (return_statement)
+ (for_statement)
+ (if_statement)
+ (function_call)
+] @statement.outer
