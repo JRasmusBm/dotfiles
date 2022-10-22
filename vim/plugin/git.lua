f1a848814301773b7a4dd2998680b52c586212e5
jrasmusbm <jrasmusbm@gmail.com>
2022-10-22 07:23:19 +0200

    refactor(diffview): load the plugin on first use

--- vim/plugin/git.lua
+++ vim/plugin/git.lua
@@ -20,3 +20,15 @@ vim.g.diffopt = require("jrasmusbm.utils.options").list {
 vim.g.fugitive_git_executable = "g"
 
 vim.g.fugitive_gitlab_domains = {}
+
+vim.api.nvim_create_user_command("DO", function(_, fargs)
+  require("jrasmusbm.diffview").ensure_setup()
+
+  vim.cmd { cmd = "DiffviewOpen", args = fargs }
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("DFH", function(_, fargs)
+  require("jrasmusbm.diffview").ensure_setup()
+
+  vim.cmd { cmd = "DiffviewFileHistory", args = fargs }
+end, { nargs = "*" })
