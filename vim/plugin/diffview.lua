7d17445c0dccefa191e8c127849b99408fb75ee2
jrasmusbm <jrasmusbm@gmail.com>
2022-10-22 13:30:22 +0200

    fix(diffview): pass fargs correctly

--- vim/plugin/diffview.lua
+++ vim/plugin/diffview.lua
@@ -1,11 +1,11 @@
-vim.api.nvim_create_user_command("DO", function(_, fargs)
+vim.api.nvim_create_user_command("DO", function(options)
   require("jrasmusbm.diffview").ensure_setup()
 
-  vim.cmd { cmd = "DiffviewOpen", args = fargs }
+  vim.cmd { cmd = "DiffviewOpen", args = options.fargs }
 end, { nargs = "*" })
 
-vim.api.nvim_create_user_command("DFH", function(_, fargs)
+vim.api.nvim_create_user_command("DFH", function(options)
   require("jrasmusbm.diffview").ensure_setup()
 
-  vim.cmd { cmd = "DiffviewFileHistory", args = fargs }
+  vim.cmd { cmd = "DiffviewFileHistory", args = options.fargs }
 end, { nargs = "*" })
