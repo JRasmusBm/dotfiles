82b9a53f5e64c441f255be50f20d75b6b3196600
jrasmusbm <jrasmusbm@gmail.com>
2022-10-27 09:08:36 +0200

    refactor(tmux): lazy load vtr

--- vim/plugin/tmux.lua
+++ vim/plugin/tmux.lua
@@ -1,10 +1,7 @@
-local mappings = require "jrasmusbm.utils.mappings"
-
-require("jrasmusbm.utils").ensure_setup("vim-tmux-navigator")
+require("jrasmusbm.utils").ensure_setup "vim-tmux-navigator"
 
 vim.cmd [[
 packadd vim-tmux-navigator
-packadd vim-tmux-runner
 ]]
 
 vim.g.vtr_filetype_runner_overrides = {
@@ -12,65 +9,57 @@ vim.g.vtr_filetype_runner_overrides = {
   go = "go run {file}",
 }
 
-vim.keymap.set({ "n" },"<localleader>tQ",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendCtrlD<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tq",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendCtrlC<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tc",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendCommandToRunner<Space>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>ta",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrAttach<Space>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>to",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrOpenRunner<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tk",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrKillRunner<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tf",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendFile<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tl",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendLinesToRunner<CR>")
-end
-)
-
-vim.keymap.set({ "n" },"<localleader>tp",function ()
-  require("jrasmusbm.utils").ensure_setup("vim-tmux-runner")
-  vim.cmd( "VtrSendCommandToRunner r<CR>")
-end
-)
-
-mappings.vmap("<localleader>tl", ":'<,'>VtrSendLinesToRunner<CR>")
-mappings.nmap("<localleader>tr", ":!tmux last-window && tmux last-pane<cr>")
-
 vim.g.VtrStripLeadingWhitespace = 0
 
-vim.defer_fn(function()
-  vim.cmd "silent VtrAttachToPane 1"
-end, 100)
+vim.keymap.set({ "n" }, "<localleader>td", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrSendCtrlD"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tc", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrSendCtrlC"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tr", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  require("jrasmusbm.utils.mappings").feedkeys ":VtrSendCommandToRunner<Space>"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>ta", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  require("jrasmusbm.utils.mappings").feedkeys ":VtrAttach<Space>"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>to", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrOpenRunner"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tk", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrKillRunner"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tf", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrSendFile"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tl", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrSendLinesToRunner"
+end)
+
+vim.keymap.set({ "n" }, "<localleader>tp", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+  vim.cmd "VtrSendCommandToRunner r"
+end)
+
+vim.keymap.set({ "v" }, "<localleader>tl", function()
+  require("jrasmusbm.utils").ensure_setup "vim-tmux-runner"
+
+  vim.cmd [[
+  '<,'>VtrSendLinesToRunner
+  ]]
+end, {})
