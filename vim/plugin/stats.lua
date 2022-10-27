2f7ee2f3e2840b033de02f2cc6c534fc2f0589a8
jrasmusbm <JRasmusBm@gmail.com>
2022-10-27 20:11:54 +0200

    refactor(vmath): uninstall
    **Why** is the change needed?
    
    Today I would use awk instead

--- vim/plugin/stats.lua
+++
@@ -1,8 +0,0 @@
-local mappings = require "jrasmusbm.utils.mappings"
-
-vim.cmd [[
-packadd vim-vmath
-]]
-
-mappings.nmap("<leader>st", ":call VMATH_Analyse()<CR>", { noremap = true })
-mappings.nmap("<leader>st", "y:call VMATH_Analyse()<CR>", { noremap = true })
