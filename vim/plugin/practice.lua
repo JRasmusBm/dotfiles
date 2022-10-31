ab1c5b4ff3db366a03ebb0bcef2dfadb3ff85dae
jrasmusbm <JRasmusBm@gmail.com>
2022-10-31 06:20:13 +0100

    refactor(hardtime): lazy load
    Closes: #497

--- vim/plugin/practice.lua
+++ vim/plugin/practice.lua
@@ -1,7 +1,3 @@
-local mappings = require "jrasmusbm.utils.mappings"
-
-require("jrasmusbm.utils").ensure_setup(" vim-hardtime")
-
 vim.api.nvim_create_user_command("VBG", function(options)
   require("jrasmusbm.utils").ensure_setup("vim-be-good")
 
@@ -13,9 +9,20 @@ vim.api.nvim_create_user_command("VBG", function(options)
   
 end, { nargs = "*" })
 
+
+vim.keymap.set({ "n" }, "<leader>ht", function ()
+if not require("jrasmusbm.utils").ensure_setup(" vim-hardtime") then
+  
 vim.g.hardtime_default_on = 1
 vim.g.hardtime_maxcount = 1
 vim.g.hardtime_timeout = 500
 vim.g.hardtime_allow_different_key = 1
+end
 
-mappings.nmap("<leader>ht", "<cmd>HardTimeToggle<CR>", { noremap = true })
+ vim.cmd[[
+ HardTimeToggle
+ ]]
+
+ 
+  
+end, {})
