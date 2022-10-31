e9790525b6db401abdfb3ce04a5ec79e4ac3c807
jrasmusbm <jrasmusbm@gmail.com>
2022-10-31 22:06:14 +0100

    refactor(vsc): lazy load

--- vim/plugin/search.lua
+++ vim/plugin/search.lua
@@ -2,7 +2,6 @@ local mappings = require "jrasmusbm.utils.mappings"
 
 vim.cmd [[
 packadd loupe
-packadd vcs-jump
 ]]
 
 vim.g.LoupeCaseSettingsAlways = 1
@@ -24,9 +23,20 @@ vim.keymap.set(
 
 vim.keymap.set({ "n" }, "<Leader> fc", "<cmd>Cheat<CR>", { noremap = true })
 
-vim.keymap.set({ "n" }, "<localleader>fd", ":VcsJump diff<Space>")
-vim.keymap.set({ "n" }, "<localleader>fg", ":VcsJump grep<Space>")
-vim.keymap.set({ "n" }, "<localleader>fm", ":VcsJump merge<Space>")
+vim.keymap.set({ "n" }, "<localleader>fd", function()
+  require("jrasmusbm.utils").ensure_setup "vcs-jump"
+  require("jrasmusbm.utils.mappings").feedkeys ":VcsJump diff "
+end, {})
+
+vim.keymap.set({ "n" }, "<localleader>fg", function()
+  require("jrasmusbm.utils").ensure_setup "vcs-jump"
+  require("jrasmusbm.utils.mappings").feedkeys ":VcsJump grep "
+end, {})
+
+vim.keymap.set({ "n" }, "<localleader>fm", function()
+  require("jrasmusbm.utils").ensure_setup "vcs-jump"
+  require("jrasmusbm.utils.mappings").feedkeys ":VcsJump merge "
+end, {})
 
 vim.keymap.set({ "n" }, "<C-O>", "<C-O>zz", { noremap = true })
 vim.keymap.set({ "n" }, "<C-I>", "<C-I>zz", { noremap = true })
