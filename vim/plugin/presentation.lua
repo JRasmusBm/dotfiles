3680c51fcade6af27ac27ff5ea22be0f3f341494
jrasmusbm <JRasmusBm@gmail.com>
2022-11-03 20:29:22 +0100

    fix(vpm):  syntax error

--- vim/plugin/presentation.lua
+++ vim/plugin/presentation.lua
@@ -4,9 +4,9 @@ require("jrasmusbm.utils").ensure_setup("vim-presentation-mode")
 vim.keymap.set({ "n" },map, "<cmd>" .. command .. "<cr>", { noremap = true })
 end
 
-with_vpm("<leader>vc", "<cmd>VpmOff<cr>"))
-with_vpm("<leader>vn", "<cmd>VpmNextHighlight<cr>"))
-with_vpm("<leader>vp", "<cmd>VpmPreviousHighlight<cr>"))
-with_vpm("<leader>vf", "<cmd>VpmOpenNextFile<cr>"))
+with_vpm("<leader>vc", "<cmd>VpmOff<cr>")
+with_vpm("<leader>vn", "<cmd>VpmNextHighlight<cr>")
+with_vpm("<leader>vp", "<cmd>VpmPreviousHighlight<cr>")
+with_vpm("<leader>vf", "<cmd>VpmOpenNextFile<cr>")
 
 vim.g.vpm_highlight_color = "gray"
