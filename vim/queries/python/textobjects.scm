d29c7806a6dc1224fcd3278667b61009fcbc8436
jrasmusbm <jrasmusbm@gmail.com>
2022-11-16 08:47:21 +0100

    feat(python): start adding statement.outer

--- vim/queries/python/textobjects.scm
+++ vim/queries/python/textobjects.scm
@@ -25,3 +25,13 @@
 
 ; }}}
 ; }}}
+
+[
+    (import_statement)
+    (import_from_statement)
+    (expression_statement)
+    (return_statement)
+    (if_statement)
+    (raise_statement)
+    (function_definition)
+] @statement.outer
