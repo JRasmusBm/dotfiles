7daae384d2c09b8234184ba0767859c07cf709f5
jrasmusbm <JRasmusBm@gmail.com>
2022-12-17 08:26:43 +0100

    fix(hs-apps): return object

--- window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
+++ window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
@@ -1,11 +1,15 @@
-hs.hotkey.bind({"cmd"}, "return", function()
-  hs.application.launchOrFocus("alacritty")
+local M = {}
+
+hs.hotkey.bind({ "cmd" }, "return", function()
+  hs.application.launchOrFocus "alacritty"
 end)
 
-hs.hotkey.bind({"cmd"}, "b", function()
-  hs.application.launchOrFocus("brave browser")
+hs.hotkey.bind({ "cmd" }, "b", function()
+  hs.application.launchOrFocus "brave browser"
 end)
 
-hs.hotkey.bind({"cmd"}, "s", function()
-  hs.application.launchOrFocus("slack")
+hs.hotkey.bind({ "cmd" }, "s", function()
+  hs.application.launchOrFocus "slack"
 end)
+
+return M
