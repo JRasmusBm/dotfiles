c00a788416dd31633ef45d4535e210395189f087
jrasmusbm <JRasmusBm@gmail.com>
2022-12-17 08:26:26 +0100

    feat(hs-window): swap windows of app with =

--- window_manager/hammerspoon/Spoons/Windows.spoon/init.lua
+++ window_manager/hammerspoon/Spoons/Windows.spoon/init.lua
@@ -38,4 +38,38 @@ hs.hotkey.bind({ "cmd", "shift" }, "z", function()
   w:toggleZoom()
 end)
 
+local cycleWindows = function(dx)
+  local currentWindow = hs.window.focusedWindow()
+  local currentApp = currentWindow:application()
+  local appname = currentApp:name()
+
+  local windows = {}
+  for i, app in ipairs { hs.application.find(appname) } do
+    for i, window in ipairs(app:allWindows()) do
+      table.insert(windows, window)
+    end
+  end
+
+  local currentWindowIndex = 0
+  for i, window in ipairs(windows) do
+    currentWindowIndex = i
+    if currentWindow:id() == window:id() then
+      break
+    end
+  end
+
+  local nextWindowIndex = ((currentWindowIndex - 1 + dx) % #windows) + 1
+  local nextWindow = windows[nextWindowIndex]
+
+  nextWindow:focus()
+end
+
+hs.hotkey.bind({ "cmd" }, "=", function()
+  cycleWindows(1)
+end)
+
+hs.hotkey.bind({ "cmd", "shift" }, "=", function()
+  cycleWindows(-1)
+end)
+
 return M
