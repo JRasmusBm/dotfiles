82fce934cc9b36d9d4debf2ad5e42dca5d343fed
jrasmusbm <JRasmusBm@gmail.com>
2022-12-27 07:06:30 +0100

    fix(git): make blame work since autoload

--- vim/plugin/git.lua
+++ vim/plugin/git.lua
@@ -1,5 +1,5 @@
-require("jrasmusbm.utils").ensure_setup("vim-gitgutter")
-require("jrasmusbm.utils").ensure_setup("nvim-web-devicons")
+require("jrasmusbm.utils").ensure_setup "vim-gitgutter"
+require("jrasmusbm.utils").ensure_setup "nvim-web-devicons"
 
 vim.cmd [[
 command! -nargs=? L silent call jrasmusbm#git#open_tree(<f-args>)
@@ -18,29 +18,37 @@ vim.g.fugitive_git_executable = "g"
 
 vim.g.fugitive_gitlab_domains = {}
 
+local bang_marker = function(options)
+  if options.bang then
+    return "!"
+  end
+  return ""
+end
+
+local enriched_command = function(options)
+  local root_cmd = "Git"
+
+  if options.fargs[1] == "browse" or options.fargs[1] == "br" then
+    table.remove(options.fargs, 1)
+    root_cmd = "GBrowse"
+  elseif options.fargs[1] == "bl" then
+    options.fargs[1] = "blame"
+  end
+
+  return root_cmd
+    .. bang_marker(options)
+    .. " "
+    .. table.concat(options.fargs, " ")
+end
+
 vim.api.nvim_create_user_command("G", function(options)
   require("jrasmusbm.utils").ensure_setup "vim-rhubarb"
   require("jrasmusbm.utils").ensure_setup "fugitive-gitlab.vim"
   require("jrasmusbm.utils").ensure_setup "vim-fugitive"
 
-  vim.cmd("Git " .. options.args)
-end, { nargs = "*" })
+  vim.cmd(enriched_command(options))
+end, { nargs = "*", bang = true })
 
-vim.api.nvim_create_user_command("GBrowse", function(options)
-  require("jrasmusbm.utils").ensure_setup "vim-rhubarb"
-  require("jrasmusbm.utils").ensure_setup "fugitive-gitlab.vim"
-  require("jrasmusbm.utils").ensure_setup "vim-fugitive"
-
-  if options.bang then
-    vim.cmd("GBrowse! " .. options.args)
-  else
-    vim.cmd("GBrowse " .. options.args)
-  end
-end, { bang = true, nargs = "*" })
-
-vim.keymap.set({ "n", "x" },"<leader>h", function ()
+vim.keymap.set({ "n", "x" }, "<leader>h", function()
   require("jrasmusbm.git").open_github_link()
-end
-
-)
-
+end)
