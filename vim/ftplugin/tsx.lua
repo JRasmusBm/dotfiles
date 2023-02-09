949f8adc669679b902de69126cf4ba072e28f950
jrasmusbm <JRasmusBm@gmail.com>
2023-02-09 06:22:52 +0100

    feat(tsx): support debug props

--- vim/ftplugin/tsx.lua
+++ vim/ftplugin/tsx.lua
@@ -22,6 +22,45 @@ ls.add_snippets("tsx", {
       { rep(1), i(2), i(1), i(3), rep(1), i(0, "null") }
     )
   ),
+
+  s(
+    { trig = "debugp", name = "debug props" },
+    fmt(
+      [[
+function logPropDifferences(newProps: any, lastProps: any) {{
+  const allKeys = new Set([
+    ...Object.keys(newProps),
+    ...Object.keys(lastProps),
+  ]);
+
+  console.log("======= RENDER ========")
+  allKeys.forEach((key) => {{
+    const newValue = newProps[key];
+    const lastValue = lastProps[key];
+    if (newValue !== lastValue) {{
+      console.log(`New Value (${{key}}): `, newValue);
+      console.log(`Last Value (${{key}}): `, lastValue);
+    }}
+  }});
+}}
+
+function useDebugPropChanges(newProps: any) {{
+  const lastProps = useRef();
+  // Should only run when the component re-mounts
+  useEffect(() => {{
+    console.log("Mounted");
+  }}, []);
+  if (lastProps.current) {{
+    logPropDifferences(newProps, lastProps.current);
+  }}
+  lastProps.current = newProps;
+}}
+
+{}
+  ]],
+      { i(0) }
+    )
+  ),
 })
 
 ls_utils.load_shared { filetype = "tsx", shared = "html" }
