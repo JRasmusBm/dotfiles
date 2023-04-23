42e8bdd770c0fd305ccd33eaa6e6228ea4621a65
jrasmusbm <JRasmusBm@gmail.com>
2023-04-23 06:58:20 +0200

    refactor: break out the command duplication

---
+++ vim/lua/jrasmusbm/utils/commands.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.mapped_args = function(options)
+  local result = {}
+
+  local args = vim.fn.join(options.fargs, " ")
+
+  if #args > 0 then
+    result.args = {}
+  end
+
+  if options.count ~= -1 then
+    result.count = options.count
+  end
+
+  if options.range ~= 0 then
+    result.range = { options.line1, options.line2 }
+  end
+
+  result.bang = options.bang
+
+  return result
+end
+
+M.run = function(command_name, options)
+  local args = M.mapped_args(options)
+
+  args.cmd = command_name
+
+  vim.cmd(args)
+end
+
+return M
--- vim/plugin/gpt.lua
+++ vim/plugin/gpt.lua
@@ -3,16 +3,5 @@ vim.api.nvim_create_user_command("Chat", function(args)
   require("jrasmusbm.utils").ensure_setup "nui.nvim"
   require("jrasmusbm.utils").ensure_setup "CodeGPT.nvim"
 
-  local new_args = {}
-
-  new_args.cmd = "Chat"
-  new_args.args = args.fargs
-
-  if args.range ~= 0 then
-    new_args.range = { args.line1, args.line2 }
-  end
-
-  new_args.bang = args.bang
-
-  vim.cmd(new_args)
+  require("jrasmusbm.utils.commands").run("Chat", args)
 end, { nargs = "*", range = true })
