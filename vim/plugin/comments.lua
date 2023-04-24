d17d9d92eaef92a909a1177736b0d0f3a5c52731
jrasmusbm <JRasmusBm@gmail.com>
2023-04-24 06:19:37 +0200

    feat(493): lazy-load Comment.nvim
    Closes: #493

--- vim/plugin/comments.lua
+++ vim/plugin/comments.lua
@@ -1,35 +1,11 @@
-local filetypes = require "jrasmusbm.filetypes"
+vim.keymap.set({ "n", "v" }, "gc", function()
+  require("jrasmusbm.comments").ensure_setup()
 
-require("jrasmusbm.utils").ensure_setup("nvim-ts-context-commentstring")
-require("jrasmusbm.utils").ensure_setup("Comment.nvim")
+  require("jrasmusbm.utils.mappings").feedkeys "gc"
+end, {})
 
-local config = {}
+vim.keymap.set({ "n" }, "gcc", function()
+  require("jrasmusbm.comments").ensure_setup()
 
-config["prehook"] = function(context)
-  -- Only calculate commentstring for tsx filetypes
-  if not vim.tbl_contains(filetypes.tsx, vim.bo.filetype) then
-    return
-  end
-  local U = require "Comment.utils"
-
-  local comment_type = {
-    [U.ctype.linewise] = "__default",
-    [U.ctype.blockwise] = "__multiline",
-  }
-
-  -- Determine the location where to calculate commentstring from
-  local location = nil
-  if context.ctype == U.ctype.blockwise then
-    location = require("ts_context_commentstring.utils").get_cursor_location()
-  elseif context.cmotion == U.cmotion.v or context.cmotion == U.cmotion.V then
-    location =
-      require("ts_context_commentstring.utils").get_visual_start_location()
-  end
-
-  return require("ts_context_commentstring.internal").calculate_commentstring {
-    key = comment_type[context.ctype],
-    location = location,
-  }
-end
-
-require("Comment").setup(config)
+  require("jrasmusbm.utils.mappings").feedkeys "gcc"
+end, {})
