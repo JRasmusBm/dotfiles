bf88d3a9ca1f24ed021016da044836b80ddadd7d
jrasmusbm <JRasmusBm@gmail.com>
2023-04-26 09:29:53 +0200

    fix: fix [t and ]t for switching tabs

--- vim/plugin/tabs.lua
+++ vim/plugin/tabs.lua
@@ -1,4 +1,4 @@
-vim.keymap.set({ "n" },"]t", "<cmd>tabn", { noremap = true })
-vim.keymap.set({ "n" },"[t", "<cmd>tabp", { noremap = true })
+vim.keymap.set({ "n" },"]t", "<cmd>tabn<cr>", { noremap = true })
+vim.keymap.set({ "n" },"[t", "<cmd>tabp<cr>", { noremap = true })
 vim.keymap.set({ "n" },"<leader>to", '<cmd>exec "tabe " . @%<cr>', { noremap = true })
 vim.keymap.set({ "n" },"<leader>tc", "<cmd>tabc<cr>", { noremap = true })
