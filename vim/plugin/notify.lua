d36ab38dd98f4d3b65c12faaf6f47f453a948682
jrasmusbm <JRasmusBm@gmail.com>
2023-04-28 06:38:20 +0200

    feat: maaping to search for messages

--- vim/plugin/notify.lua
+++ vim/plugin/notify.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_user_command("Messages", function()
 
   require("telescope").extensions.notify.notify()
 end, { nargs = "*" })
+
+vim.keymap.set({ "n" }, "<leader>fm", function ()
+  require("jrasmusbm.telescope").ensure_setup()
+
+  require("telescope").extensions.notify.notify()
+end
+
+, {  })
+
