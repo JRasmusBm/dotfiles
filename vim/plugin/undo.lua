5b4e47d401400e903ec618f59a246fa817722e51
jrasmusbm <JRasmusBm@gmail.com>
2023-04-28 08:52:32 +0200

    refactor: shorter undo-tree keymap

--- vim/plugin/undo.lua
+++ vim/plugin/undo.lua
@@ -1,8 +1,11 @@
 vim.opt.undodir = vim.fn.stdpath "data" .. "/undodir"
 vim.opt.undofile = true
 
-vim.api.nvim_create_user_command("UndotreeToggle", function(options)
+local handler = function(options)
   require("jrasmusbm.utils").ensure_setup "undotree"
 
   require("jrasmusbm.utils.commands").run("UndotreeToggle", options)
-end, { nargs = "*" })
+end
+
+vim.api.nvim_create_user_command("Undo", handler, { nargs = "*" })
+vim.api.nvim_create_user_command("Un", handler, { nargs = "*" })
