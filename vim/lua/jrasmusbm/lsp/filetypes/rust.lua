b607f170a5d3a258567cc2e41567c3d2d8b92c6c
jrasmusbm <JRasmusBm@gmail.com>
2023-05-02 07:18:46 +0200

    feat: use rust-analyzer

--- vim/lua/jrasmusbm/lsp/filetypes/rust.lua
+++ vim/lua/jrasmusbm/lsp/filetypes/rust.lua
@@ -1,17 +1,11 @@
 local M = {}
 
 M.setup = function(options)
-  require("lspconfig").rls.setup {
+  require("lspconfig").rust_analyzer.setup {
     capabilities = options.capabilities,
     on_attach = options.on_attach,
     filetypes = require("jrasmusbm.filetypes").rust,
-    settings = {
-      rust = {
-        unstable_features = true,
-        build_on_save = false,
-        all_features = true,
-      },
-    },
+    cmd={"rustup", "run", "stable", "rust-analyzer"}
   }
 end
 
