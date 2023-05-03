f1b1b0668e027a302475771ac116d1e9885198e1
jrasmusbm <JRasmusBm@gmail.com>
2023-05-03 09:39:26 +0200

    feat: return body as default

--- bin-src/git-commit-section/src/main.rs
+++ bin-src/git-commit-section/src/main.rs
@@ -1,34 +1,92 @@
+use itertools::Itertools;
 use std::{
     env::args,
     process::{exit, Command},
 };
 
-fn get_inputs() -> String {
-    let Ok(output) = Command::new("git").args(["local-main-branch"]).output() else {
+trait InputWrapper {
+    fn get_commits(&self) -> String;
+    fn get_inputs(&self) -> Vec<String>;
+}
+
+struct RealInputs {}
+
+impl InputWrapper for RealInputs {
+    fn get_commits(&self) -> String {
+        let Ok(output) = Command::new("git").args(["local-main-branch"]).output() else {
         exit(1)
     };
 
-    let commit_range = format!("{}..HEAD", String::from_utf8_lossy(&output.stdout).trim());
+        let commit_range = format!("{}..HEAD", String::from_utf8_lossy(&output.stdout).trim());
 
-    let Ok(output) = Command::new("git").args(["--no-pager", "log", "--reverse", "--format=%b", &commit_range]).output() else {
+        let Ok(output) = Command::new("git").args(["--no-pager", "log", "--reverse", "--format=%b", &commit_range]).output() else {
         exit(1)
     };
 
-    return String::from_utf8_lossy(&output.stdout).trim().to_string();
+        return String::from_utf8_lossy(&output.stdout).trim().to_string();
+    }
+
+    fn get_inputs(&self) -> Vec<String> {
+        return args().into_iter().filter(|a| !a.contains("-")).collect();
+    }
+}
+
+fn extract_section(input_wrapper: &dyn InputWrapper) -> String {
+    let result = input_wrapper.get_commits();
+
+    return result
+        .split("\n")
+        .skip(2)
+        .map(|s| s.trim())
+        .intersperse("\n")
+        .collect();
 }
 
 fn main() {
-    let result = get_inputs();
-
-    let Some(header_from) = args().into_iter().filter(|a| a != "-p" && a != "-r").nth(1);
-    let header_to = args().into_iter().filter(|a| a != "-p" && a != "-r").nth(2);
-
-    let result = result.split(header_from.chars()).reduce(|acc, cur| {
-        acc.to_owned().push_str(&cur);
-        return acc;
-    });
+    let result = extract_section(&RealInputs {});
 
     if args().into_iter().any(|a| a == "-p") {
         println!("{:?}", result);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    struct FakeInputs {
+        commits: String,
+        inputs: Vec<String>,
+    }
+
+    impl InputWrapper for FakeInputs {
+        fn get_inputs(&self) -> Vec<String> {
+            return self.inputs.clone();
+        }
+
+        fn get_commits(&self) -> String {
+            return self.commits.clone();
+        }
+    }
+
+    #[test]
+    fn returns_whole_body_if_there_are_no_inputs() {
+        let inputs = Vec::new();
+        let commits = "feat: This is a commit without headings
+
+        It was created to ensure that without inputs,
+        extract_section returns the whole body of the commit"
+            .to_string();
+
+        assert_eq!(
+            extract_section(&FakeInputs {
+                inputs: inputs,
+                commits: commits
+            }),
+            "
+It was created to ensure that without inputs,
+extract_section returns the whole body of the commit
+        "
+            .trim()
+        );
     }
 }
