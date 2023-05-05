1336028a766da3d49279561a5119af1937052966
jrasmusbm <JRasmusBm@gmail.com>
2023-05-05 23:14:23 +0200

    fix: load filetypes on start

--- vim/init.lua
+++ vim/init.lua
@@ -1,5 +1,6 @@
 require("jrasmusbm.utils").ensure_setup("impatient.nvim", true)
 require("jrasmusbm.utils").ensure_setup("filetype.nvim", true)
+require("jrasmusbm.change_filetype").setup()
 
 require('impatient')
 
--- vim/plugin/change_filetype.lua
+++
@@ -1,1 +0,0 @@
-require("jrasmusbm.change_filetype").setup()
