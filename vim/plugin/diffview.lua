7044c5515b0fdf1da7e55c1f1f1aabc3f77d48ca
jrasmusbm <JRasmusBm@gmail.com>
2023-05-11 06:04:49 +0200

    feat: run diffview against the merge-base
    If you diff against the other branch HEAD you will get a lot of
    unrelated changes in your diff.

--- vim/plugin/diffview.lua
+++ vim/plugin/diffview.lua
@@ -3,7 +3,9 @@ vim.api.nvim_create_user_command("DO", function(options)
   local args = options.fargs
 
   if #options.fargs <= 0 then
-    args = { vim.trim(vim.fn.system "git local-main-branch") }
+    args = {
+      vim.trim(vim.fn.system "git merge-base `git local-main-branch` HEAD"),
+    }
   end
 
   vim.cmd { cmd = "DiffviewOpen", args = args }
