adb0bb95ce89d3ec0d44b81133399ed00c5e2259
jrasmusbm <JRasmusBm@gmail.com>
2023-06-11 12:47:19 +0200

    fix: use correct rust-analyzer on termux

--- vim/lua/jrasmusbm/lsp/filetypes/rust.lua
+++ vim/lua/jrasmusbm/lsp/filetypes/rust.lua
@@ -1,11 +1,17 @@
 local M = {}
 
+local cmd = {"rustup", "run", "stable", "rust-analyzer"}
+
+if require("jrasmusbm.compat").in_termux() then
+  cmd = {"rust-analyzer"}
+end
+
 M.setup = function(options)
   require("lspconfig").rust_analyzer.setup {
     capabilities = options.capabilities,
     on_attach = options.on_attach,
     filetypes = require("jrasmusbm.filetypes").rust,
-    cmd={"rustup", "run", "stable", "rust-analyzer"}
+    cmd=cmd
   }
 end
 
