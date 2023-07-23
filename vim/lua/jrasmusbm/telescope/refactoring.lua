a88c443afe9f1a7145ba73f83722d2c53606a41c
jrasmusbm <JRasmusBm@gmail.com>
2023-07-23 08:24:29 +0200

    style: format

--- vim/lua/jrasmusbm/telescope/refactoring.lua
+++ vim/lua/jrasmusbm/telescope/refactoring.lua
@@ -1,36 +1,35 @@
-M = {}
+local M = {}
 
 local function refactor(prompt_bufnr)
-  local content = require("telescope.actions.state").get_selected_entry(
-    prompt_bufnr
-  )
+  local content =
+      require("telescope.actions.state").get_selected_entry(prompt_bufnr)
   require("telescope.actions").close(prompt_bufnr)
   require("refactoring").refactor(content.value)
 end
 
 M.refactors = function()
   require("jrasmusbm.telescope").ensure_setup()
-  if require("jrasmusbm.utils").ensure_setup("refactoring.nvim") then
-    
-  require("refactoring").setup()
+  if not require("jrasmusbm.utils").ensure_setup "refactoring.nvim" then
+    return
   end
 
-  
-
+  require("refactoring").setup()
 
   local opts = require("telescope.themes").get_cursor() -- set personal telescope options
-  require("telescope.pickers").new(opts, {
-    prompt_title = "refactors",
-    finder = require("telescope.finders").new_table {
-      results = require("refactoring").get_refactors(),
-    },
-    sorter = require("telescope.config").values.generic_sorter(opts),
-    attach_mappings = function(_, map)
-      map("i", "<CR>", refactor)
-      map("n", "<CR>", refactor)
-      return true
-    end,
-  }):find()
+  require("telescope.pickers")
+      .new(opts, {
+        prompt_title = "refactors",
+        finder = require("telescope.finders").new_table {
+          results = require("refactoring").get_refactors(),
+        },
+        sorter = require("telescope.config").values.generic_sorter(opts),
+        attach_mappings = function(_, map)
+          map("i", "<CR>", refactor)
+          map("n", "<CR>", refactor)
+          return true
+        end,
+      })
+      :find()
 end
 
 return M
