ed1c35e205cad38a85ef6ea6806cec70e9341b8b
jrasmusbm <JRasmusBm@gmail.com>
2023-07-31 09:10:48 +0200

    feat: add spotify

--- window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
+++ window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
@@ -1,55 +1,24 @@
 local M = {}
 
 local apps = {
-  [1] = "brave_browser",
+  [1] = "brave browser",
   [2] = "alacritty",
   [3] = "slack",
   [8] = "obs",
-  [9] = "kdelive",
+  [9] = "kdenlive",
+  [0] = "spotify",
 }
 
--- for key, app in pairs(apps) do
---   if app ~= nil then
---     hs.hotkey.bind({ "ctrl" }, key, function()
---       hs.application.launchOrFocus(app)
---     end)
---   end
--- end
+for key, app in pairs(apps) do
+  if app ~= nil then
+    hs.hotkey.bind({ "ctrl" }, tostring(key), function()
+      hs.application.launchOrFocus(app)
+    end)
 
--- BROWSER
-
-hs.hotkey.bind({ "ctrl" }, "1", function()
-  hs.application.launchOrFocus "brave browser"
-end)
-
--- TERMINAL
-
-hs.hotkey.bind({ "ctrl" }, "2", function()
-  hs.application.launchOrFocus "alacritty"
-end)
-
--- SLACK
-
-hs.hotkey.bind({ "ctrl" }, "3", function()
-  hs.application.launchOrFocus "slack"
-end)
-
--- OBS
-
-hs.hotkey.bind({ "ctrl" }, "8", function()
-  hs.application.launchOrFocus "obs"
-end)
-
--- KDENLIVE
-
-hs.hotkey.bind({ "ctrl" }, "9", function()
-  hs.application.launchOrFocus "kdenlive"
-end)
-
--- OS
-
-hs.hotkey.bind({ "cmd" }, "e", function()
-  hs.application.launchOrFocus "finder"
-end)
+    hs.hotkey.bind({ "ctrl", "shift" }, tostring(key), function()
+      hs.application.launch(app)
+    end)
+  end
+end
 
 return M
