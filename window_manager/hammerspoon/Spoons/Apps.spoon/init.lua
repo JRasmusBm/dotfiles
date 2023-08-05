3a4f1d3632603b08e8183bf1fcdd951a440625cf
jrasmusbm <JRasmusBm@gmail.com>
2023-08-05 21:37:37 +0200

    fix: support cmd-e

--- window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
+++ window_manager/hammerspoon/Spoons/Apps.spoon/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local apps = {
+local ctrl_apps = {
   [1] = "brave browser",
   [2] = "alacritty",
   [3] = "slack",
@@ -9,13 +9,29 @@ local apps = {
   [0] = "spotify",
 }
 
-for key, app in pairs(apps) do
+local cmd_apps = {
+  e = "finder",
+}
+
+for key, app in pairs(ctrl_apps) do
   if app ~= nil then
     hs.hotkey.bind({ "ctrl" }, tostring(key), function()
       hs.application.launchOrFocus(app)
     end)
 
     hs.hotkey.bind({ "ctrl", "shift" }, tostring(key), function()
+      hs.application.launch(app)
+    end)
+  end
+end
+
+for key, app in pairs(cmd_apps) do
+  if app ~= nil then
+    hs.hotkey.bind({ "cmd" }, tostring(key), function()
+      hs.application.launchOrFocus(app)
+    end)
+
+    hs.hotkey.bind({ "cmd", "shift" }, tostring(key), function()
       hs.application.launch(app)
     end)
   end
