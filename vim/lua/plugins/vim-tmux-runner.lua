6ddf3ac6b9540fbb1a110f8558444cdaa7cb45e8
jrasmusbm <JRasmusBm@gmail.com>
2024-05-11 22:06:56 +0200

    fix: fix vim-tmux-runner issues

--- vim/lua/plugins/vim-tmux-runner.lua
+++ vim/lua/plugins/vim-tmux-runner.lua
@@ -1,14 +1,16 @@
+vim.g.VtrPercentage = 45
+vim.g.VtrOrientation = "h"
+vim.g.vtr_filetype_runner_overrides = {
+  rust = "cargo run {file}",
+  go = "go run {file}",
+  terraform = "terraform apply",
+}
+
 return {
   "christoomey/vim-tmux-runner",
   lazy = true,
   config = function()
-    vim.g.VtrPercentage = 45
-    vim.g.VtrOrientation = "h"
-    vim.g.vtr_filetype_runner_overrides = {
-      rust = "cargo run {file}",
-      go = "go run {file}",
-      terraform = "terraform apply",
-    }
+    require("jrasmusbm.tmux").setup()
 
     vim.cmd [[
   silent VtrAttachToPane 1"
