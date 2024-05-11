dc0c7d0134cf67786f392a0b5e6b6689347a63d8
jrasmusbm <JRasmusBm@gmail.com>
2024-05-11 22:07:05 +0200

    fix: vim peculiar

--- vim/lua/plugins/vim-peculiar.lua
+++ vim/lua/plugins/vim-peculiar.lua
@@ -3,6 +3,11 @@ return {
   lazy = true,
   config = function()
     vim.g["peculiar#suppress_highlight_n"] = 1
+
+    vim.keymap.set({ "n" }, "<localleader>v", "<Plug>PeculiarV")
+    vim.keymap.set({ "n" }, "<localleader>g", "<Plug>PeculiarG")
+    vim.keymap.set({ "n" }, "<localleader>n", "<Plug>PeculiarN")
+    vim.keymap.set({ "n" }, "<localleader>r", "<Plug>PeculiarR")
   end,
   keys = {
     "<localleader>v",
--- vim/plugin/peculiar.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set({ "n" }, "<localleader>v", "<Plug>PeculiarV")
-vim.keymap.set({ "n" }, "<localleader>g", "<Plug>PeculiarG")
-vim.keymap.set({ "n" }, "<localleader>n", "<Plug>PeculiarN")
-vim.keymap.set({ "n" }, "<localleader>r", "<Plug>PeculiarR")
