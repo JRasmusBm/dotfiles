dcb3734e03cd53ea15bc7784afc2be0549c579b6
jrasmusbm <JRasmusBm@gmail.com>
2024-05-17 09:23:48 +0200

    refactor: move nonvim to own file

--- vim/plugin/nonvim.lua
+++ vim/plugin/nonvim.lua
@@ -1,6 +1,14 @@
-vim.cmd [[
-augroup Nonvim
-  au BufRead *.pdf sil exe '!open ' . shellescape(expand('%:p')) | call jrasmusbm#utils#close_file()
-  au BufRead *.png,*.jpg sil exe '!xdg-open ' . shellescape(expand('%:p')) | call jrasmusbm#utils#close_file()
-augroup END
-]]
+local nonvim_augroup = vim.api.nvim_create_augroup("nonvim", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufNew", "BufNewFile", "BufRead" }, {
+  group = nonvim_augroup,
+  pattern = { "*.pdf", "*.png", "*.jpg" },
+  callback = function(opts)
+    if vim.loop.os_uname().sysname == "Darwin" then
+      vim.system { "open", opts.file }
+    else
+      vim.system { "xdg-open", opts.file }
+    end
+    vim.api.nvim_buf_delete(0, { force = true })
+  end,
+})
