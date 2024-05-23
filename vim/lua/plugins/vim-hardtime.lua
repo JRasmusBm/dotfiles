71eb068c990b5b2307d871679733ae9a2eba4e06
jrasmusbm <JRasmusBm@gmail.com>
2024-05-23 11:53:45 +0200

    feat: use new hardtime

--- vim/lua/plugins/vim-hardtime.lua
+++ vim/lua/plugins/vim-hardtime.lua
@@ -1,11 +1,5 @@
 return {
-  "takac/vim-hardtime",
-  lazy = true,
-  config = function()
-    vim.g.hardtime_default_on = 1
-    vim.g.hardtime_maxcount = 1
-    vim.g.hardtime_timeout = 500
-    vim.g.hardtime_allow_different_key = 1
-  end,
-  cmd = { "HardTimeToggle" },
+  "m4xshen/hardtime.nvim",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {}
 }
