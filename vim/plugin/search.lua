b06950a95e538b1f4d41c444bcd10027088f8408
jrasmusbm <JRasmusBm@gmail.com>
2024-05-23 20:38:06 +0200

    feat: add mapping for lazy regex mapping

--- vim/plugin/search.lua
+++ vim/plugin/search.lua
@@ -1,4 +1,5 @@
-vim.keymap.set({ "n" }, "gs", ':%s//g<Left><Left>')
-vim.keymap.set({ "v" }, "gs", ':s//g<Left><Left>')
+vim.keymap.set({ "n" }, "gs", ":%s//g<Left><Left>")
+vim.keymap.set({ "v" }, "gs", ":s//g<Left><Left>")
 
 vim.keymap.set({ "n" }, "<Leader> fc", "<cmd>Cheat<CR>", { noremap = true })
+vim.keymap.set({ "c" }, "<c-j>", "{-}", { noremap = true })
