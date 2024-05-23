64a59b3d21041d2a34d0c28aa032372e356ff857
jrasmusbm <JRasmusBm@gmail.com>
2024-05-23 21:47:18 +0200

    refactor: move help to lua

--- vim/after/ftplugin/help.lua
+++ vim/after/ftplugin/help.lua
@@ -1,1 +1,19 @@
-vim.schedule(vim.fn["jrasmusbm#help#help_mode"])
+vim.schedule(function()
+  vim.keymap.set(
+    { "n" },
+    "<tab>",
+    ":call search('|.\\{\\-\\}|')<cr>:noh<cr>2l",
+    { buffer = 0 }
+  )
+  vim.keymap.set(
+    { "n" },
+    "<s-tab>",
+    "F|:call search('|.\\{\\-\\}|', 'b')<cr>:noh<cr>2l",
+    { buffer = 0 }
+  )
+  vim.keymap.set({ "n" }, "<cr>", "<c-]>", { buffer = true })
+  vim.keymap.set({ "n" }, "<bs>", "<c-T>", { buffer = true })
+  vim.keymap.set({ "n" }, "q", ":q<CR>", { buffer = true })
+  vim.opt_local.number = false
+  vim.cmd.only()
+end)
--- vim/autoload/jrasmusbm/help.vim
+++
@@ -1,9 +0,0 @@
-function! jrasmusbm#help#help_mode()
-  nnoremap <buffer> <tab> :call search('\|.\{-}\|', 'w')<cr>:noh<cr>2l
-  nnoremap <buffer> <S-tab> F\|:call search('\|.\{-}\|', 'wb')<cr>:noh<cr>2l
-  nnoremap <buffer> <cr> <c-]>
-  nnoremap <buffer> <bs> <c-T>
-  nnoremap <buffer> q :q<CR>
-  setlocal nonumber
-  only
-endfunction
