637ec8ec6cd2d79eba497543bcf95ea01c35f11a
jrasmusbm <JRasmusBm@gmail.com>
2024-05-23 07:38:49 +0200

    fix: make yanks work on termux

--- vim/plugin/yanking.lua
+++ vim/plugin/yanking.lua
@@ -1,14 +1,12 @@
-if not require("jrasmusbm.compat").in_termux() then
-  vim.keymap.set({ "n" },"<C-Y>", '"+y', { noremap = true })
-  vim.keymap.set({ "n" },"<C-P>", '"+p', { noremap = true })
-  vim.keymap.set({ "v" },"<C-Y>", '"+y', { noremap = true })
-  vim.keymap.set({ "v" },"<C-P>", '"+p', { noremap = true })
-end
+vim.keymap.set({ "n" },"<C-Y>", '"+y', { noremap = true })
+vim.keymap.set({ "n" },"<C-P>", '"+p', { noremap = true })
+vim.keymap.set({ "v" },"<C-Y>", '"+y', { noremap = true })
+vim.keymap.set({ "v" },"<C-P>", '"+p', { noremap = true })
 
 vim.cmd [[
 augroup ShowYank
-  if exists('##TextYankPost')
-    autocmd TextYankPost * lua vim.highlight.on_yank {higroup='IncSearch', timeout=200}
+if exists('##TextYankPost')
+  autocmd TextYankPost * lua vim.highlight.on_yank {higroup='IncSearch', timeout=200}
   endif
-augroup END
-]]
+  augroup END
+  ]]
