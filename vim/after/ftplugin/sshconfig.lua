a51541cf8720816a6d8105a274fbdc429d036c9b
jrasmusbm <JRasmusBm@gmail.com>
2024-05-24 22:44:11 +0200

    refactor: migrate sshconfig snippets to lua

--- vim/after/ftplugin/sshconfig.lua
+++ vim/after/ftplugin/sshconfig.lua
@@ -1,5 +1,25 @@
-vim.cmd [[
-setlocal foldmethod=marker
+vim.opt_local.foldmethod = "marker"
 
-execute "source $DOTFILES/vim/lua/jrasmusbm/snippets/shared/sshconfig.vim"
-]]
+vim.keymap.set({ "n" }, "zf", function()
+  require("jrasmusbm.folds").fold_selection()
+end, { buffer = true })
+
+local ls = require "luasnip"
+local s = ls.s
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+
+ls.add_snippets("sshconfig", {
+  s(
+    { trig = "fo", name = "fold open" },
+    fmt(
+      [[
+# {} {{{{{{
+{}
+# }}}}}}
+{}
+ ]],
+      { i(1), i(2), i(0) }
+    )
+  ),
+})
--- vim/lua/jrasmusbm/snippets/shared/sshconfig.vim
+++
@@ -1,12 +0,0 @@
-" Folds {{{
-" New {{{
-inoremap <buffer> <leader>fo # <++> {{{
-      \S<++>
-      \# }}}
-      \S<++>:call bushels#backward_marker(3) 
-"}}}
-" Selection {{{
-nnoremap <buffer> <expr> zf lua require("jrasmusbm.folds").fold_selection()
-"}}}
-"}}}
-
