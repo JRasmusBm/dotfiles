bfbc3021013c2b4de6ebeb19735e76a3f3304677
jrasmusbm <JRasmusBm@gmail.com>
2024-05-24 22:14:44 +0200

    refactor: migrate conf snippets to lua

--- vim/after/ftplugin/conf.lua
+++ vim/after/ftplugin/conf.lua
@@ -1,7 +1,3 @@
-vim.cmd [[
-execute "source $DOTFILES/vim/lua/jrasmusbm/snippets/shared/conf.vim"
-]]
-
 vim.opt_local.foldmethod = "marker"
 
 if vim.fn.expand "%:t" == "termux.conf" then
@@ -9,3 +5,27 @@ if vim.fn.expand "%:t" == "termux.conf" then
     vim.cmd "!termux-reload-settings"
   end, { buffer = 0 })
 end
+
+local ls = require "luasnip"
+local s = ls.s
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+
+vim.keymap.set({ "n" }, "zf", function()
+  require("jrasmusbm.folds").fold_selection()
+end, { buffer = true })
+
+ls.add_snippets("conf", {
+  s(
+    { trig = "fo", name = "fold open" },
+    fmt(
+      [[
+# {} {{{{{{
+{}
+# }}}}}}
+{}
+ ]],
+      { i(1), i(2), i(0) }
+    )
+  ),
+})
