0ed9d2df5a952fa47f78efad72e2332d3477ae57
jrasmusbm <JRasmusBm@gmail.com>
2024-05-24 22:46:28 +0200

    refactor: migrate dockerfile snippets to lua

--- vim/after/ftplugin/dockerfile.lua
+++ vim/after/ftplugin/dockerfile.lua
@@ -1,3 +1,17 @@
-vim.cmd [[
-execute "source $DOTFILES/vim/lua/jrasmusbm/snippets/shared/dockerfile.vim"
-]]
+local ls = require "luasnip"
+local s = ls.s
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+
+ls.add_snippets("dockerfile", {
+  s(
+    { trig = "f", name = "from" },
+    fmt(
+      [[
+FROM {}:{}
+
+{}]],
+      { i(1), i(2), i(0) }
+    )
+  ),
+})
--- vim/lua/jrasmusbm/snippets/shared/dockerfile.vim
+++
@@ -1,8 +0,0 @@
-" Image {{{
-inoremap <buffer> <leader>df FROM <++>:<++>
-  \<++>:call bushels#backward_marker(3)
-" }}}
-" COPY {{{
-inoremap <buffer> <leader>co COPY <++> <++>
-  \<++>:call bushels#backward_marker(3)
-" }}}
