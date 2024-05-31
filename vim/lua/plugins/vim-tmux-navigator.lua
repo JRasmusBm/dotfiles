2b7f02f4a3f7f3534c61c0faef78c92f72841143
jrasmusbm <JRasmusBm@gmail.com>
2024-05-31 20:51:20 +0200

    feat: use vim-tmux-navigator port

--- vim/lua/plugins/vim-tmux-navigator.lua
+++ vim/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,15 @@
 return {
-  "christoomey/vim-tmux-navigator",
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    local nvim_tmux_nav = require "nvim-tmux-navigation"
+
+    nvim_tmux_nav.setup {}
+
+    vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+  end,
 }
