d834f8f1ea0ac1759710ca73e62e2889a0763cdb
jrasmusbm <JRasmusBm@gmail.com>
2025-06-23 12:11:19 +0200

    fix: window switching

--- window_manager/hammerspoon/Spoons/CycleWindows.spoon/init.lua
+++ window_manager/hammerspoon/Spoons/CycleWindows.spoon/init.lua
@@ -3,22 +3,16 @@ local M = {}
 local cycleWindows = function(dx)
   local currentWindow = hs.window.focusedWindow()
   local currentApp = currentWindow:application()
-  local appname = currentApp:name()
 
-  local windows = {}
-  for i, app in ipairs { hs.application.find(appname) } do
-    for i, window in ipairs(app:allWindows()) do
-      table.insert(windows, window)
-    end
-  end
+  local windows = hs.fnutils.filter(currentApp:allWindows(), function(window)
+    return window:isStandard()
+  end)
 
-  local currentWindowIndex = 0
-  for i, window in ipairs(windows) do
-    currentWindowIndex = i
-    if currentWindow:id() == window:id() then
-      break
-    end
-  end
+  table.sort(windows, function(a, b)
+    return a:id() < b:id()
+  end)
+
+  local currentWindowIndex = hs.fnutils.indexOf(windows, currentWindow)
 
   local nextWindowIndex = ((currentWindowIndex - 1 + dx) % #windows) + 1
   local nextWindow = windows[nextWindowIndex]
