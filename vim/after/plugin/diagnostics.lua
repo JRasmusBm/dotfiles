9058896675a5c29031951b6fdf1ad228416bae40
jrasmusbm <JRasmusBm@gmail.com>
2025-11-17 09:40:10 +0100

    feat: move diagnostics out of lsp

--- vim/after/plugin/diagnostics.lua
+++ vim/after/plugin/diagnostics.lua
@@ -8,3 +8,35 @@ augroup LspLocationList
   autocmd User DiagnosticChanged lua vim.diagnostic.setloclist({ open=false })
 augroup END
 ]]
+
+local opts = { noremap = true, silent = true, buffer = 0 }
+
+vim.keymap.set({ "n" }, "g?", vim.diagnostic.open_float, opts)
+
+vim.keymap.set({ "n" }, "[v", function()
+  vim.diagnostic.jump {
+    count = -1,
+    severity = { min = vim.diagnostic.severity.ERROR },
+  }
+end, opts)
+
+vim.keymap.set({ "n" }, "]v", function()
+  vim.diagnostic.jump {
+    count = 1,
+    severity = { min = vim.diagnostic.severity.ERROR },
+  }
+end, opts)
+
+vim.keymap.set({ "n" }, "[V", function()
+  vim.diagnostic.jump {
+    count = -1,
+    severity = { max = vim.diagnostic.severity.WARN },
+  }
+end, opts)
+
+vim.keymap.set({ "n" }, "]V", function()
+  vim.diagnostic.jump {
+    count = 1,
+    severity = { max = vim.diagnostic.severity.WARN },
+  }
+end, opts)
--- vim/lua/jrasmusbm/lsp/attach.lua
+++ vim/lua/jrasmusbm/lsp/attach.lua
@@ -17,77 +17,47 @@ function M.on_attach(client, bufnr)
   -- Mappings.
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set({ "n" }, "g?", vim.diagnostic.open_float, opts)
-
-  vim.keymap.set({ "n" }, "[v", function()
-    vim.diagnostic.jump {
-      count = -1,
-      severity = { min = vim.diagnostic.severity.ERROR },
-    }
-  end, opts)
-
-  vim.keymap.set({ "n" }, "]v", function()
-    vim.diagnostic.jump {
-      count = 1,
-      severity = { min = vim.diagnostic.severity.ERROR },
-    }
-  end, opts)
-
-  vim.keymap.set({ "n" }, "[V", function()
-    vim.diagnostic.jump {
-      count = -1,
-      severity = { max = vim.diagnostic.severity.WARN },
-    }
-  end, opts)
-
-  vim.keymap.set({ "n" }, "]V", function()
-    vim.diagnostic.jump {
-      count = 1,
-      severity = { max = vim.diagnostic.severity.WARN },
-    }
-  end, opts)
-
   vim.keymap.set({ "n" }, "<leader>aq", function()
     vim.diagnostic.setloclist { open = true }
   end, opts)
 
-  if client.supports_method "textDocument/declaration" then
+  if client:supports_method "textDocument/declaration" then
     vim.keymap.set({ "n" }, "gD", vim.lsp.buf.declaration)
   end
 
-  if client.supports_method "textDocument/definition" then
+  if client:supports_method "textDocument/definition" then
     vim.keymap.set({ "n" }, "<c-]>", vim.lsp.buf.definition)
   end
 
-  if client.supports_method "textDocument/hover" then
+  if client:supports_method "textDocument/hover" then
     vim.keymap.set({ "n" }, "K", vim.lsp.buf.hover)
   end
 
-  if client.supports_method "textDocument/implementation" then
+  if client:supports_method "textDocument/implementation" then
     vim.keymap.set({ "n" }, "gi", vim.lsp.buf.implementation)
   end
 
-  if client.supports_method "textDocument/signatureHelp" then
+  if client:supports_method "textDocument/signatureHelp" then
     vim.keymap.set({ "n" }, "gK", vim.lsp.buf.signature_help)
   end
 
-  if client.supports_method "textDocument/typeDefinition" then
+  if client:supports_method "textDocument/typeDefinition" then
     vim.keymap.set({ "n" }, "gt", vim.lsp.buf.type_definition)
   end
 
-  if client.supports_method "textDocument/rename" then
+  if client:supports_method "textDocument/rename" then
     vim.keymap.set({ "n" }, "g/", vim.lsp.buf.rename)
   end
 
-  if client.supports_method "textDocument/references" then
+  if client:supports_method "textDocument/references" then
     vim.keymap.set({ "n" }, "gr", require("telescope.builtin").lsp_references)
   end
 
-  if client.supports_method "textDocument/codeAction" then
+  if client:supports_method "textDocument/codeAction" then
     vim.keymap.set({ "n", "v" }, "<leader>af", vim.lsp.buf.code_action)
   end
 
-  if client.supports_method "workspace/symbol" then
+  if client:supports_method "workspace/symbol" then
     vim.keymap.set(
       { "n" },
       "<leader>as",
@@ -101,7 +71,7 @@ function M.on_attach(client, bufnr)
     )
   end
 
-  if client.supports_method "textDocument/document_formatting" then
+  if client:supports_method "textDocument/document_formatting" then
     vim.keymap.set({ "n" }, "==", function()
       vim.lsp.buf.format { async = true }
     end)
@@ -114,7 +84,7 @@ function M.on_attach(client, bufnr)
   end
 
 
-  if client.supports_method "textDocument/document_range_formatting" then
+  if client:supports_method "textDocument/document_range_formatting" then
     vim.keymap.set({ "v" }, "==", vim.lsp.buf.format)
   end
 
@@ -136,7 +106,7 @@ function M.on_attach(client, bufnr)
   )
 
   -- Set autocommands conditional on server_capabilities
-  if client.supports_method "textDocument/documentHighlight" then
+  if client:supports_method "textDocument/documentHighlight" then
     local lsp_document_highlight_augroup =
         vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
 
