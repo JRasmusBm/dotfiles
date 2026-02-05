80013d9aa154ac1efbfe8f1ce72d7fac1e2ade6a
jrasmusbm <JRasmusBm@gmail.com>
2026-02-05 10:13:23 +0100

    fix(vim): auto-clean stale shada temp files on startup

--- vim/after/plugin/history.lua
+++ vim/after/plugin/history.lua
@@ -1,6 +1,20 @@
+local shada_path = vim.fn.expand("$HOME/.config/nvim/shada")
+
 vim.opt.shada = require("jrasmusbm.utils.options").list {
   "'0",
   "<0",
   "f0",
-  "n$HOME/.config/nvim/shada",
+  "n" .. shada_path,
 }
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local stale_files = vim.fn.glob(shada_path .. ".tmp.*", false, true)
+    for _, file in ipairs(stale_files) do
+      local age_seconds = os.time() - vim.fn.getftime(file)
+      if age_seconds > 3600 then
+        vim.fn.delete(file)
+      end
+    end
+  end,
+})
